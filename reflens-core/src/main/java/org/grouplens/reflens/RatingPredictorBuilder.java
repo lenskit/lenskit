6191996e553bbcb3341fa701aa0382ed45213d63
Michael Ekstrand <michael@elehack.net>
2010-11-06 09:31:05 -0500

    Implement constant-value baseline predictor

---
+++ reflens-core/src/main/java/org/grouplens/reflens/RatingPredictorBuilder.java
@@ -0,0 +1,44 @@
+/* RefLens, a reference implementation of recommender algorithms.
+ * Copyright 2010 Michael Ekstrand <ekstrand@cs.umn.edu>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package org.grouplens.reflens;
+
+import org.grouplens.reflens.data.UserRatingProfile;
+import org.grouplens.reflens.util.DataSource;
+
+public interface RatingPredictorBuilder<U,I> {
+
+	public RatingPredictor<U, I> build(DataSource<UserRatingProfile<U, I>> data);
+	
+	public static class RecEngineBuilderWrapper<U,I> implements RatingPredictorBuilder<U, I> {
+		private final RecommenderBuilder<U, I> builder;
+
+		public RecEngineBuilderWrapper(RecommenderBuilder<U, I> builder) {
+			this.builder = builder;
+		}
+		
+		public RatingPredictor<U, I> build(DataSource<UserRatingProfile<U, I>> data) {
+			RecommendationEngine<U, I> engine = builder.build(data);
+			RatingPredictor<U, I> pred = engine.getRatingPredictor();
+			if (pred != null)
+				return pred;
+			else
+				throw new RuntimeException("Recommender does not support rating prediction");
+		}
+	}
+}
---
+++ reflens-core/src/main/java/org/grouplens/reflens/baseline/ConstantBaselinePredictor.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package org.grouplens.reflens.baseline;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Map;
+
+import org.grouplens.reflens.RatingPredictor;
+import org.grouplens.reflens.RatingPredictorBuilder;
+import org.grouplens.reflens.data.ScoredObject;
+import org.grouplens.reflens.data.UserRatingProfile;
+import org.grouplens.reflens.util.DataSource;
+
+import com.google.inject.BindingAnnotation;
+import com.google.inject.Inject;
+
+/**
+ * @author Michael Ekstrand <ekstrand@cs.umn.edu>
+ *
+ */
+public class ConstantBaselinePredictor<U, I> implements RatingPredictor<U, I> {
+	private final double value;
+	
+	@BindingAnnotation
+	@Target({ElementType.PARAMETER, ElementType.FIELD, ElementType.METHOD})
+	@Retention(RetentionPolicy.RUNTIME)
+	public static @interface Value {
+		public static final String PROPERTY_NAME
+			= "org.grouplens.reflens.baseline.constant.value";
+	}
+	
+	private ConstantBaselinePredictor(double value) {
+		this.value = value;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.grouplens.reflens.RatingPredictor#predict(java.lang.Object, java.util.Map, java.lang.Object)
+	 */
+	@Override
+	public ScoredObject<I> predict(U user, Map<I, Double> ratings, I item) {
+		return new ScoredObject<I>(item, value);
+	}
+
+	public static class Builder<U,I> implements RatingPredictorBuilder<U, I> {
+		private final double value;
+		@Inject
+		public Builder(@Value double value) {
+			this.value = value;
+		}
+		@Override
+		public RatingPredictor<U, I> build(
+				DataSource<UserRatingProfile<U, I>> data) {
+			return new ConstantBaselinePredictor<U, I>(value);
+		}
+	}
+}
