bdb4edfb4f89ab3962757e2d20a3941d58bb2e5b
hg <unknown>
2011-01-12 16:59:11 -0600

    Make Identity double function final.

--- reflens-core/src/main/java/org/grouplens/reflens/util/DoubleFunction.java
+++ reflens-core/src/main/java/org/grouplens/reflens/util/DoubleFunction.java
@@ -3,7 +3,7 @@ package org.grouplens.reflens.util;
 public interface DoubleFunction {
 	double apply(double v);
 	
-	public static class Identity implements DoubleFunction {
+	public static final class Identity implements DoubleFunction {
 		public double apply(double v) {
 			return v;
 		}
