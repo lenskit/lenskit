96a9d8763fd5c6d21fb1ac4e5d5ec9fdd4a59d35
Michael Ekstrand <ekstrand@cs.umn.edu>
2011-02-15 17:13:52 -0600

    Implement a set based on sorted arrays (refs #41)

---
+++ reflens-core/src/main/java/org/grouplens/reflens/util/LongSortedArraySet.java
@@ -0,0 +1,257 @@
+/*
+ * Copyright 2011 Michael Ekstrand <ekstrand@cs.umn.edu>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.grouplens.reflens.util;
+
+import it.unimi.dsi.fastutil.longs.AbstractLongBidirectionalIterator;
+import it.unimi.dsi.fastutil.longs.AbstractLongSortedSet;
+import it.unimi.dsi.fastutil.longs.LongArraySet;
+import it.unimi.dsi.fastutil.longs.LongBidirectionalIterator;
+import it.unimi.dsi.fastutil.longs.LongComparator;
+import it.unimi.dsi.fastutil.longs.LongComparators;
+import it.unimi.dsi.fastutil.longs.LongIterators;
+import it.unimi.dsi.fastutil.longs.LongSortedSet;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.NoSuchElementException;
+
+/**
+ * A sorted set of longs implemented using a sorted array.  It's much faster
+ * than {@link LongArraySet} as it is able to use binary searches.  The set
+ * is also immutable.
+ * 
+ * No orders are supported other than the natural ordering.
+ * 
+ * @author Michael Ekstrand <ekstrand@cs.umn.edu>
+ *
+ */
+public final class LongSortedArraySet extends AbstractLongSortedSet {
+	private final long[] data;
+	private final int start, end;
+	
+	public LongSortedArraySet(Collection<Long> items) {
+		this(LongIterators.unwrap(LongIterators.asLongIterator(items.iterator())));
+	}
+	
+	/**
+	 * Create a new set from an existing array.
+	 * @param items An array of items. The array will be sorted and used as the
+	 * backing store for the set. If this array is changed after creating the
+	 * set, behavior is undefined.
+	 * @see #LongSortedArraySet(long[], int, int)
+	 */
+	public LongSortedArraySet(long[] items) {
+		this(items, 0, items.length);
+	}
+	
+	/**
+	 * Create a new set from a range of an existing array.
+	 * @param items An array of items. The array will be sorted and used as the
+	 * backing store for the set. If this array is changed after creating the
+	 * @param fromIndex The index of the first item in the array to use.
+	 * @param toIndex The end of the array to use (last index + 1).
+	 * set, behavior is undefined.
+	 * @throws IndexOutOfBoundsException if <var>start</var> or <var>end</var>
+	 * is out of range.
+	 */
+	public LongSortedArraySet(long[] items, int fromIndex, int toIndex) {
+		this(items, fromIndex, toIndex, false);
+	}
+	
+	/**
+	 * Create a new set from a range of an existing array.
+	 * @param items An array of items. The array will be sorted and used as the
+	 * backing store for the set. If this array is changed after creating the
+	 * @param fromIndex The index of the first item in the array to use.
+	 * @param toIndex The end of the array to use (last index + 1).
+	 * set, behavior is undefined.
+	 * @param sorted Assume the array is sorted.
+	 * @throws IndexOutOfBoundsException if <var>start</var> or <var>end</var>
+	 * is out of range.
+	 */
+	private LongSortedArraySet(long[] items, int fromIndex, int toIndex, boolean sorted) {
+		data = items;
+		start = fromIndex;
+		end = toIndex;
+		if (fromIndex < 0 || toIndex > data.length)
+			throw new IndexOutOfBoundsException();
+		// check for sortedness first to avoid the actual sort
+		if (!sorted && !isSorted())
+			Arrays.sort(data, start, end);
+	}
+	
+	/**
+	 * Check that the array is sorted.
+	 * @return <code>true</code> iff the array is sorted.
+	 */
+	private boolean isSorted() {
+		for (int i = start; i < end - 1; i++) {
+			if (data[i] > data[i+1]) return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Find the index for a key.
+	 * @see Arrays#binarySearch(long[], int, int, long)
+	 * @param key
+	 * @return
+	 */
+	private int findIndex(long key) {
+		return Arrays.binarySearch(data, start, end, key);
+	}
+	
+	/**
+	 * Find the index where <var>key</var> would appear if it exists.
+	 * @param key The search key.
+	 * @return The index in the array of the key, if it exists; otherwise, the
+	 * index of the first element greater than <var>key</var> (or the end of the
+	 * array).
+	 */
+	private int findIndexAlways(long key) {
+		int i = findIndex(key);
+		if (i < 0)
+			i = -(i+1);
+		return i;
+	}
+
+	/* (non-Javadoc)
+	 * @see it.unimi.dsi.fastutil.longs.LongSortedSet#comparator()
+	 */
+	@Override
+	public LongComparator comparator() {
+		return LongComparators.NATURAL_COMPARATOR;
+	}
+
+	/* (non-Javadoc)
+	 * @see it.unimi.dsi.fastutil.longs.LongSortedSet#firstLong()
+	 */
+	@Override
+	public long firstLong() {
+		if (end - start > 0)
+			return data[start];
+		else
+			throw new NoSuchElementException();
+	}
+
+	/* (non-Javadoc)
+	 * @see it.unimi.dsi.fastutil.longs.LongSortedSet#headSet(long)
+	 */
+	@Override
+	public LongSortedSet headSet(long key) {
+		int nend = findIndexAlways(key);
+		return new LongSortedArraySet(data, start, nend, true);
+	}
+
+	/* (non-Javadoc)
+	 * @see it.unimi.dsi.fastutil.longs.LongSortedSet#iterator(long)
+	 */
+	@Override
+	public LongBidirectionalIterator iterator(long key) {
+		int index = findIndexAlways(key);
+		if (index < end && data[index] == key)
+			index++;
+		return new IterImpl(index);
+	}
+	
+	private final class IterImpl extends AbstractLongBidirectionalIterator {
+		private int pos;
+		public IterImpl(int start) {
+			pos = start;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return pos < end;
+		}
+
+		@Override
+		public boolean hasPrevious() {
+			return pos > start;
+		}
+		
+		@Override
+		public long nextLong() {
+			if (hasNext())
+				return data[pos++];
+			else
+				throw new NoSuchElementException();
+		}
+		
+		@Override
+		public long previousLong() {
+			if (hasPrevious())
+				return data[--pos];
+			else
+				throw new NoSuchElementException();
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see it.unimi.dsi.fastutil.longs.LongSortedSet#lastLong()
+	 */
+	@Override
+	public long lastLong() {
+		if (end - start > 0)
+			return data[end-1];
+		else
+			throw new NoSuchElementException();
+	}
+
+	/* (non-Javadoc)
+	 * @see it.unimi.dsi.fastutil.longs.LongSortedSet#subSet(long, long)
+	 */
+	@Override
+	public LongSortedSet subSet(long startKey, long endKey) {
+		return new LongSortedArraySet(data, findIndexAlways(startKey), findIndexAlways(endKey), true);
+	}
+
+	/* (non-Javadoc)
+	 * @see it.unimi.dsi.fastutil.longs.LongSortedSet#tailSet(long)
+	 */
+	@Override
+	public LongSortedSet tailSet(long key) {
+		return new LongSortedArraySet(data, findIndexAlways(key), end, true);
+	}
+
+	/* (non-Javadoc)
+	 * @see it.unimi.dsi.fastutil.longs.AbstractLongSortedSet#iterator()
+	 */
+	@Override
+	public LongBidirectionalIterator iterator() {
+		return new IterImpl(start);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.AbstractCollection#size()
+	 */
+	@Override
+	public int size() {
+		return end - start;
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		return end == start;
+	}
+
+}
---
+++ reflens-core/src/test/java/org/grouplens/reflens/util/TestLongSortedArraySet.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2011 Michael Ekstrand <ekstrand@cs.umn.edu>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.grouplens.reflens.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import it.unimi.dsi.fastutil.longs.LongArrayList;
+import it.unimi.dsi.fastutil.longs.LongBidirectionalIterator;
+import it.unimi.dsi.fastutil.longs.LongIterators;
+import it.unimi.dsi.fastutil.longs.LongSortedSet;
+
+import java.util.Collections;
+
+import org.junit.Test;
+
+
+/**
+ * @author Michael Ekstrand <ekstrand@cs.umn.edu>
+ *
+ */
+public class TestLongSortedArraySet {
+	@SuppressWarnings("unchecked")
+	private LongSortedArraySet emptySet() {
+		return new LongSortedArraySet(Collections.EMPTY_LIST);
+	}
+	
+	@Test
+	public void testEmptySet() {
+		LongSortedArraySet set = emptySet();
+		assertTrue(set.isEmpty());
+		assertEquals(0, set.size());
+		LongBidirectionalIterator iter = set.iterator();
+		assertNotNull(iter);
+		assertFalse(iter.hasNext());
+		assertFalse(iter.hasPrevious());
+		assertFalse(set.contains(42));
+		assertFalse(set.contains(Long.valueOf(42)));
+	}
+	
+	@Test
+	public void testEmptySetSubset() {
+		LongSortedArraySet set = emptySet();
+		LongSortedSet sset = set.headSet(50);
+		assertTrue(sset.isEmpty());
+		assertEquals(0, sset.size());
+		assertFalse(set.iterator().hasNext());
+		assertFalse(set.iterator().hasPrevious());
+		assertFalse(set.contains(42));
+		assertFalse(set.contains(Long.valueOf(42)));
+	}
+	
+	private void testSetSimple(LongSortedSet set) {
+		assertFalse(set.isEmpty());
+		assertEquals(3, set.size());
+		assertEquals(2, set.firstLong());
+		assertEquals(6, set.lastLong());
+		assertEquals(Long.valueOf(2), set.first());
+		assertEquals(Long.valueOf(6), set.last());
+		assertTrue(set.contains(2));
+		assertTrue(set.contains(5));
+		assertTrue(set.contains(6));
+		assertFalse(set.iterator().hasPrevious());
+		long[] items = LongIterators.unwrap(set.iterator());
+		assertEquals(2, items[0]);
+		assertEquals(5, items[1]);
+		assertEquals(6, items[2]);
+	}
+	
+	@Test
+	public void testArrayCtor() {
+		LongSortedSet set = new LongSortedArraySet(new long[]{2, 5, 6});
+		testSetSimple(set);
+		
+		LongBidirectionalIterator iter = set.iterator();
+		assertTrue(iter.hasNext());
+		assertEquals(2, iter.nextLong());
+		assertTrue(iter.hasPrevious());
+		assertEquals(2, iter.previousLong());
+		assertFalse(iter.hasPrevious());
+	}
+	@Test
+	public void testArrayCtorUnsorted() {
+		long[] data = {5, 2, 6};
+		LongSortedSet set = new LongSortedArraySet(data);
+		testSetSimple(set);
+		// make sure it tweaked our array
+		assertEquals(2, data[0]);
+	}
+	@Test
+	public void testArrayCtorRanged() {
+		LongSortedSet set = new LongSortedArraySet(new long[]{42, 5, 2, 6, 7}, 1, 4);
+		testSetSimple(set);
+	}
+	@Test
+	public void testCollectionCtor() {
+		long[] data = {5, 2, 6};
+		LongSortedSet set = new LongSortedArraySet(new LongArrayList(data));
+		testSetSimple(set);
+	}
+	
+	@Test
+	public void testHeadSet() {
+		long[] data = {7, 5, 2, 6, 42};
+		LongSortedSet set = new LongSortedArraySet(data);
+		assertEquals(data.length, set.size());
+		testSetSimple(set.headSet(7));
+	}
+	
+	@Test
+	public void testTailSet() {
+		long[] data = {0, 5, 2, 6, 1};
+		LongSortedSet set = new LongSortedArraySet(data);
+		assertEquals(data.length, set.size());
+		testSetSimple(set.tailSet(2));
+	}
+	
+	@Test
+	public void testSubSet() {
+		long[] data = {0, 42, 5, 2, 6, 1, 7};
+		LongSortedSet set = new LongSortedArraySet(data);
+		assertEquals(data.length, set.size());
+		testSetSimple(set.subSet(2, 7));
+	}
+	
+	@Test
+	public void testTailIter() {
+		long[] data = {0, 42, 5, 2, 6, 1, 7};
+		LongSortedSet set = new LongSortedArraySet(data);
+		LongBidirectionalIterator iter = set.iterator(2);
+		assertTrue(iter.hasNext());
+		assertTrue(iter.hasPrevious());
+		assertEquals(5, iter.nextLong());
+		iter = set.iterator(2);
+		assertEquals(2, iter.previousLong());
+		iter = set.iterator(-5);
+		assertFalse(iter.hasPrevious());
+		iter = set.iterator(100);
+		assertFalse(iter.hasNext());
+		iter = set.iterator(3);
+		assertEquals(5, iter.nextLong());
+		iter = set.iterator(3);
+		assertEquals(2, iter.previousLong());
+	}
+}
