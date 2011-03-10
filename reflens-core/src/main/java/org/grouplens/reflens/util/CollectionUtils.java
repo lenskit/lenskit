e4c0f5b06dad90a0b732eac2a6b51d0555e784ab
Michael Ekstrand <ekstrand@cs.umn.edu>
2011-03-10 16:16:35 -0600

    Make fast collection wrapping more complete.

--- reflens-core/src/main/java/org/grouplens/reflens/util/CollectionUtils.java
+++ reflens-core/src/main/java/org/grouplens/reflens/util/CollectionUtils.java
@@ -23,11 +23,15 @@ package org.grouplens.reflens.util;
 
 import it.unimi.dsi.fastutil.longs.AbstractLongCollection;
 import it.unimi.dsi.fastutil.longs.AbstractLongIterator;
+import it.unimi.dsi.fastutil.longs.AbstractLongSet;
 import it.unimi.dsi.fastutil.longs.LongCollection;
 import it.unimi.dsi.fastutil.longs.LongIterator;
+import it.unimi.dsi.fastutil.longs.LongIterators;
+import it.unimi.dsi.fastutil.longs.LongSet;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Various helper methods for working with collections (particularly Fastutil
@@ -44,25 +48,150 @@ public class CollectionUtils {
 	 * @return The collection as a {@link LongCollection}.
 	 */
 	public static LongCollection fastCollection(final Collection<Long> longs) {
-		if (longs instanceof LongCollection) return (LongCollection) longs;
+		if (longs instanceof LongCollection)
+			return (LongCollection) longs;
+		else
+			return new LongCollectionWrapper(longs);
+	}
+	
+	/**
+	 * Get a Fastutil {@link LongSet} from a {@link Set} of longs.
+	 */
+	public static LongSet fastSet(final Set<Long> longs) {
+		if (longs instanceof LongSet)
+			return (LongSet) longs;
+		else
+			return new LongSetWrapper(longs);
+	}
+	
+	/**
+	 * Wrapper class that implements a {@link LongCollection} by delegating to
+	 * a {@link Collection}.
+	 * @author Michael Ekstrand <ekstrand@cs.umn.edu>
+	 *
+	 */
+	static class LongCollectionWrapper implements LongCollection {
+		final protected Collection<Long> base;
+		LongCollectionWrapper(Collection<Long> base) {
+			this.base = base;
+		}
+		@Override
+		public int size() {
+			return base.size();
+		}
 		
-		return new AbstractLongCollection() {
-			private Collection<Long> base = longs;
-			@Override
-			public int size() {
-				return base.size();
-			}
-			
-			@Override
-			public boolean contains(long key) {
-				return base.contains(key);
-			}
-			
-			@Override
-			public LongIterator iterator() {
-				return fastIterator(base.iterator());
-			}
-		};
+		@Override
+		public boolean contains(long key) {
+			return base.contains(key);
+		}
+		
+		@Override
+		public LongIterator iterator() {
+			return LongIterators.asLongIterator(base.iterator());
+		}
+		
+		@Override
+		public boolean add(Long item) {
+			return base.add(item);
+		}
+		
+		@Override
+		public boolean addAll(Collection<? extends Long> items) {
+			return base.addAll(items);
+		}
+		@Override
+		public void clear() {
+			base.clear();
+		}
+		@Override
+		public boolean contains(Object item) {
+			return base.contains(item);
+		}
+		@Override
+		public boolean containsAll(Collection<?> items) {
+			return base.containsAll(items);
+		}
+		@Override
+		public boolean isEmpty() {
+			return base.isEmpty();
+		}
+		@Override
+		public boolean remove(Object item) {
+			return base.remove(item);
+		}
+		@Override
+		public boolean removeAll(Collection<?> items) {
+			return base.removeAll(items);
+		}
+		@Override
+		public boolean retainAll(Collection<?> items) {
+			return base.retainAll(items);
+		}
+		@Override
+		public Object[] toArray() {
+			return base.toArray();
+		}
+		@Override
+		public LongIterator longIterator() {
+			return iterator();
+		}
+		@Override
+		public <T> T[] toArray(T[] a) {
+			return base.toArray(a);
+		}
+		@Override
+		public long[] toLongArray() {
+			long[] items = new long[size()];
+			LongIterators.unwrap(iterator(), items);
+			return items;
+		}
+		@Override
+		public long[] toLongArray(long[] a) {
+			if (a.length < size())
+				a = new long[size()];
+			LongIterators.unwrap(iterator(), a);
+			return a;
+		}
+		@Override
+		public long[] toArray(long[] a) {
+			return toLongArray(a);
+		}
+		@Override
+		public boolean add(long key) {
+			return base.add(key);
+		}
+		@Override
+		public boolean rem(long key) {
+			return base.remove(key);
+		}
+		@Override
+		public boolean addAll(LongCollection c) {
+			return base.addAll(c);
+		}
+		@Override
+		public boolean containsAll(LongCollection c) {
+			return base.containsAll(c);
+		}
+		@Override
+		public boolean removeAll(LongCollection c) {
+			return base.removeAll(c);
+		}
+		@Override
+		public boolean retainAll(LongCollection c) {
+			return base.retainAll(c);
+		}
+	}
+	
+	static class LongSetWrapper extends LongCollectionWrapper implements LongSet {
+		LongSetWrapper(Collection<Long> base) {
+			super(base);
+		}
+
+		@Override
+		public boolean remove(long k) {
+			return super.rem(k);
+		}
+		
 	}
 	
 	/**
@@ -73,6 +202,7 @@ public class CollectionUtils {
 	 * @param col The collection of longs.
 	 * @return A Fastutil iterator for the collection.
 	 */
+	@Deprecated
 	public static LongIterator fastIterator(final Collection<Long> col) {
 		return fastIterator(col.iterator());
 	}
@@ -84,20 +214,10 @@ public class CollectionUtils {
 	 * is already a Fastutil iterator (an instance of {@link LongIterator}), this
 	 * is simply <var>iter</var> cast to {@link LongIterator}.  Otherwise, it is
 	 * a wrapper object.
+	 * @deprecated Use {@link LongIterators#asLongIterator(Iterator)} instead.
 	 */
+	@Deprecated
 	public static LongIterator fastIterator(final Iterator<Long> iter) {
-		if (iter instanceof LongIterator) return (LongIterator) iter;
-		
-		return new AbstractLongIterator() {
-			Iterator<Long> biter = iter;
-			@Override
-			public boolean hasNext() {
-				return biter.hasNext(); 
-			}
-			@Override
-			public Long next() {
-				return biter.next();
-			}
-		};
+		return LongIterators.asLongIterator(iter);
 	}
 }
