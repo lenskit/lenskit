ca2ec7512e0fad9423b7b9520dc1c308bc7d3ab7
Michael Ekstrand <ekstrand@cs.umn.edu>
2011-03-28 15:17:25 -0500

    Update user-user script to provide an implementation

--- lenskit-knn/etc/useruser.js
+++ lenskit-knn/etc/useruser.js
@@ -1,4 +1,5 @@
  rec.name = "UserUser"
  rec.module = org.grouplens.lenskit.knn.user.UserRecommenderModule
+ rec.module.variant = org.grouplens.lenskit.knn.user.SimpleUserUserRatingRecommender
  rec.module.knn.similarityDamping = 50
  rec.module.core.baseline = org.grouplens.lenskit.baseline.ItemUserMeanPredictor
