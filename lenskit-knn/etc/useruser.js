e1dc5551f8b92ad76220912c63b7319c95efa497
Michael Ekstrand <ekstrand@cs.umn.edu>
2011-05-26 17:38:35 -0500

    Update demo JavaScript files.

--- lenskit-knn/etc/useruser.js
+++ lenskit-knn/etc/useruser.js
@@ -1,7 +1,25 @@
+recipe.addEval("Coverage");
+recipe.addEval("NDCG");
+recipe.addEval("HLUtility");
+recipe.addEval("MAE");
+recipe.addEval("RMSE");
+
+importPackage(org.grouplens.lenskit);
+importPackage(org.grouplens.lenskit.params);
+importPackage(org.grouplens.lenskit.baseline);
+importPackage(org.grouplens.lenskit.norm);
+importPackage(org.grouplens.lenskit.knn.user);
+importPackage(org.grouplens.lenskit.knn.params);
+
+var rec = recipe.addAlgorithm();
 rec.name = "UserUser";
-rec.builder = new org.grouplens.lenskit.knn.user.UserUserRecommenderEngineBuilder();
-rec.builder.neighborhoodFinder = new org.grouplens.lenskit.knn.user.SimpleNeighborhoodFinder.Builder();
-rec.builder.neighborhoodFinder.neighborhoodSize = 30;
-rec.builder.normalizer = new org.grouplens.lenskit.norm.BaselineSubtractingNormalizer.Builder();
-rec.builder.normalizer.baselinePredictor = new org.grouplens.lenskit.baseline.UserMeanPredictor.Builder();
-rec.builder.neighborhoodFinder.normalizer = rec.builder.normalizer
+rec.preload = true;
+
+rec.factory.setComponent(RatingPredictor, UserUserRatingPredictor);
+rec.factory.setComponent(NeighborhoodFinder, SimpleNeighborhoodFinder);
+rec.factory.setComponent(PredictNormalizer, UserRatingVectorNormalizer, UserVarianceNormalizer);
+rec.factory.setComponent(BaselinePredictor, ItemUserMeanPredictor);
+rec.factory.setComponent(NormalizerBaseline, BaselinePredictor, UserMeanPredictor);
+rec.factory.setComponent(UserRatingVectorNormalizer, BaselineSubtractingNormalizer);
+rec.factory.set(SimilarityDamping, 100);
+rec.factory.set(NeighborhoodSize, 30);
