49d7e1904d096ca6d61a458bd7710f260ee88082
Michael Ekstrand <ekstrand@cs.umn.edu>
2011-08-01 11:38:01 -0500

    Move TaskTimer to the lenskit-core package.

--- lenskit-core/src/main/java/org/grouplens/lenskit/util/TaskTimer.java
+++ lenskit-core/src/main/java/org/grouplens/lenskit/util/TaskTimer.java
@@ -16,7 +16,7 @@
  * this program; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-package org.grouplens.lenskit.eval;
+package org.grouplens.lenskit.util;
 
 /**
  * @author Michael Ekstrand <ekstrand@cs.umn.edu>
