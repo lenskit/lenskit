40622b89e10556ac97c1de880fd1af3e79b53501
Michael Ekstrand <ekstrand@cs.umn.edu>
2011-08-12 14:17:52 -0500

    Document Indexer.

--- lenskit-core/src/main/java/org/grouplens/lenskit/data/Indexer.java
+++ lenskit-core/src/main/java/org/grouplens/lenskit/data/Indexer.java
@@ -24,38 +24,75 @@ import it.unimi.dsi.fastutil.longs.LongArrayList;
 import it.unimi.dsi.fastutil.longs.LongList;
 import it.unimi.dsi.fastutil.longs.LongLists;
 
+/**
+ * Build contiguous 0-based indexes for long IDs.
+ * 
+ * @author Michael Ekstrand <ekstrand@cs.umn.edu>
+ *
+ */
 public class Indexer implements Index {
     private static final long serialVersionUID = -8383883342128686850L;
 
     private Long2IntMap indexes;
     private LongArrayList ids;
 
+    /**
+     * Construct a new empty indexer.  The first interned ID will have index 0.
+     */
     public Indexer() {
         indexes = new Long2IntOpenHashMap();
         indexes.defaultReturnValue(-1);
         ids = new LongArrayList();
     }
 
+    /**
+     * Get the ID for an index.
+     * @param idx The index to query.
+     * @return The ID with index <var>idx</var>.
+     * @throws IndexOutOfBoundsException if the index is invalid.
+     */
     @Override
     public long getId(int idx) {
         return ids.getLong(idx);
     }
 
+    /**
+     * Get the list of all IDs in index order.
+     * @return The list of IDs.
+     */
     @Override
     public LongList getIds() {
         return LongLists.unmodifiable(ids);
     }
 
+    /**
+     * Get the index for an ID.
+     * 
+     * @param id The ID o query.
+     * @return The index assigned to <var>id</var>, or -1 if <var>id</var> has
+     *         not been interned.
+     */
     @Override
     public int getIndex(long id) {
         return indexes.get(id);
     }
 
+    /**
+     * Get the number of objects interned in this indexer.
+     */
     @Override
     public int getObjectCount() {
         return ids.size();
     }
 
+    /**
+     * Get an index for an ID, generating a new one if necessary.
+     * 
+     * @param id The ID.
+     * @return The index for <var>id</var>. If the ID has already been interned,
+     *         the old index is returned; otherwise, a new index is generated
+     *         and returned.
+     */
     public int internId(long id) {
         int idx = getIndex(id);
         if (idx < 0) {
