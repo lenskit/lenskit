db38c3383e6090b8df1545faf4fb4a87b1a0fb22
Michael Ekstrand <ekstrand@cs.umn.edu>
2012-01-31 13:35:26 -0600

    More tests for the step range quantizer.

--- lenskit-core/src/test/java/org/grouplens/lenskit/norm/TestStepRangeQuantizer.java
+++ lenskit-core/src/test/java/org/grouplens/lenskit/norm/TestStepRangeQuantizer.java
@@ -1,7 +1,7 @@
 package org.grouplens.lenskit.norm;
 
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
 import org.junit.Before;
@@ -16,5 +16,13 @@ public class TestStepRangeQuantizer {
         double[] vals = StepRangeQuantizer.makeValues(0.5, 5.0, 0.5);
         double[] evals = { 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0 };
         assertArrayEquals(evals, vals, 1.0e-6);
+    }
+
+    @Test
+    public void testHalfStars() {
+        Quantizer q = new StepRangeQuantizer(0.5, 5.0, 0.5);
+        assertThat(q.getValue(q.apply(4.9)), closeTo(5.0, 1.0e-6));
+        assertThat(q.getValue(q.apply(4.7)), closeTo(4.5, 1.0e-6));
+        assertThat(q.getValue(q.apply(3.42)), closeTo(3.5, 1.0e-6));
     }
 }
