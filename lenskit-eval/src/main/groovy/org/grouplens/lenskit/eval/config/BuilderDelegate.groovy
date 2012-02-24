314d9eacdf67cf9a474b091223268cfdcf11bc8a
Michael Ekstrand <ekstrand@cs.umn.edu>
2012-02-24 16:06:52 -0600

    Document builder delegate.

--- lenskit-eval/src/main/groovy/org/grouplens/lenskit/eval/config/BuilderDelegate.groovy
+++ lenskit-eval/src/main/groovy/org/grouplens/lenskit/eval/config/BuilderDelegate.groovy
@@ -3,9 +3,22 @@ package org.grouplens.lenskit.eval.config
 import org.apache.commons.lang3.builder.Builder
 
 /**
- * Basic builder delegate, configures an evaluation builder. It pretends methods
- * based on the methods provided by a particular {@link Builder} (detected via
- * reflection)
+ * Default Groovy delegate for configuring builders of evaluator components. It wraps
+ * a {@link Builder}, and dispatches methods as follows:
+ * <p/>
+ * To resolve "foo", this delegate first looks for a method "setFoo", then "addFoo", such that one
+ * of the following holds:
+ * <ul>
+ *     <li>The method takes the parameters specified.</li>
+ *     <li>The parameters specified can be converted into appropriate types
+ *     for the method by wrapping strings with {@code File} objects and instantiating
+ *     classes with their default constructors.</li>
+ *     <li>The method takes a single parameter annotated with the {@link DefaultBuilder}
+ *     annotation. This builder is constructed using a constructor that matches the arguments
+ *     provided, except that the last argument is ommitted if it is a {@link Closure}. If the
+ *     last argument is a {@link Closure}, it is used to configure the builder with an appropriate
+ *     delegate before the object is built.</li>
+ * </ul>
  *
  * @author Michael Ekstrand
  * @since 0.10
--- lenskit-eval/src/test/groovy/org/grouplens/lenskit/eval/data/TestCSVFileConfig.groovy
+++ lenskit-eval/src/test/groovy/org/grouplens/lenskit/eval/data/TestCSVFileConfig.groovy
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.*
 import org.junit.Test
 
 import org.grouplens.lenskit.eval.config.ConfigTestBase
+import org.grouplens.lenskit.data.pref.PreferenceDomain
 
 /**
  * @author Michael Ekstrand
@@ -48,5 +49,19 @@ class TestCSVFileConfig extends ConfigTestBase {
         }
         assertThat(source.name, equalTo("ml-100k/u.data"))
         assertThat(source.file, equalTo(new File("ml-100k/u.data")))
+    }
+
+    @Test
+    void testDomain() {
+        def source = eval {
+            csvfile("ml-100k") {
+                domain {
+                    minimum 1.0
+                    maximum 5.0
+                    precision 1.0
+                }
+            }
+        }
+        assertThat(source.domain, equalTo(new PreferenceDomain(1.0, 5.0, 1.0)))
     }
 }
