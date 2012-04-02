3056c015bf3ebae800928d63dfc24ec99f7aeb04
Matthias Balke <matthias.balke@tu-dortmund.de>
2012-04-02 18:23:13 +0200

    added MovingAverage

---
+++ lenskit-data-structures/src/main/java/org/grouplens/lenskit/statistics/MovingAverage.java
@@ -0,0 +1,81 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2011 Regents of the University of Minnesota
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package org.grouplens.lenskit.statistics;
+
+/**
+ * <p>
+ * A simple implementation of a moving average.<br/>
+ * Only the average value and the item count is stored, not the values it self.
+ * This is ideal, for saving memory on large data sets.
+ * </p>
+ * <p>
+ * The removal of values is also supported. But be careful not to remove values
+ * that have never been added. Most of the time this will lead to unwanted
+ * results!
+ * </p>
+ * 
+ * @author Matthias.Balke <matthias.balke@tu-dortmund.de>
+ * @since 0.10
+ * 
+ */
+public class MovingAverage {
+
+    private double average;
+    private long count;
+
+    public MovingAverage() {
+        count = 0;
+        average = 0;
+    }
+
+    public MovingAverage(long count, double average) {
+        this.count = count;
+        this.average = average;
+    }
+
+    public synchronized void add(double value) {
+        if (count == 0) {
+            average = value;
+            count++;
+        } else {
+            average = ((average * count) + value) / ++count;
+        }
+    }
+
+    public synchronized void remove(double value) {
+        if (count == 0) {
+            throw new AssertionError("no value to remove");
+        } else {
+            average = ((average * count) - value) / --count;
+        }
+    }
+
+    public synchronized double getAverage() {
+        return average;
+    }
+
+    public synchronized long getCount() {
+        return count;
+    }
+
+    @Override
+    public synchronized String toString() {
+        return Double.toString(average);
+    }
+}
---
+++ lenskit-data-structures/src/test/java/org/grouplens/lenskit/statistics/MovingAverageTest.java
@@ -0,0 +1,158 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2011 Regents of the University of Minnesota
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package org.grouplens.lenskit.statistics;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Matthias.Balke <matthias.balke@tu-dortmund.de>
+ * @since 0.10
+ * 
+ */
+public class MovingAverageTest {
+
+    private static final double PRECISION = 0.0001;
+
+    /**
+     * Test method for
+     * {@link org.grouplens.lenskit.statistics.MovingAverage#add(double)} .
+     */
+    @Test
+    public void testAdd() {
+        MovingAverage avg = new MovingAverage();
+        Assert.assertEquals(0.0, avg.getAverage(), PRECISION);
+        Assert.assertEquals(0, avg.getCount());
+
+        avg.add(10);
+        Assert.assertEquals(10, avg.getAverage(), PRECISION);
+        Assert.assertEquals(1, avg.getCount());
+
+        avg.add(3);
+        Assert.assertEquals(6.5, avg.getAverage(), PRECISION);
+        Assert.assertEquals(2, avg.getCount());
+
+        avg.add(7);
+        Assert.assertEquals(6.666666, avg.getAverage(), PRECISION);
+        Assert.assertEquals(3, avg.getCount());
+
+        avg.add(13);
+        Assert.assertEquals(8.25, avg.getAverage(), PRECISION);
+        Assert.assertEquals(4, avg.getCount());
+
+        avg.add(21);
+        Assert.assertEquals(10.8, avg.getAverage(), PRECISION);
+        Assert.assertEquals(5, avg.getCount());
+    }
+
+    /**
+     * Test method for
+     * {@link org.grouplens.lenskit.statistics.MovingAverage#remove(double)} .
+     */
+    @Test
+    public void testRemove() {
+        MovingAverage avg = new MovingAverage();
+        Assert.assertEquals(0.0, avg.getAverage(), PRECISION);
+        Assert.assertEquals(0, avg.getCount());
+
+        avg.add(2);
+        Assert.assertEquals(2, avg.getAverage(), PRECISION);
+        Assert.assertEquals(1, avg.getCount());
+
+        avg.add(2);
+        Assert.assertEquals(2, avg.getAverage(), PRECISION);
+        Assert.assertEquals(2, avg.getCount());
+
+        avg.add(5);
+        Assert.assertEquals(3, avg.getAverage(), PRECISION);
+        Assert.assertEquals(3, avg.getCount());
+
+        avg.remove(2);
+        Assert.assertEquals(3.5, avg.getAverage(), PRECISION);
+        Assert.assertEquals(2, avg.getCount());
+
+        avg.add(21);
+        Assert.assertEquals(9.33333333, avg.getAverage(), PRECISION);
+        Assert.assertEquals(3, avg.getCount());
+
+        avg.remove(5);
+        Assert.assertEquals(11.5, avg.getAverage(), PRECISION);
+        Assert.assertEquals(2, avg.getCount());
+    }
+
+    /**
+     * Test method for
+     * {@link org.grouplens.lenskit.statistics.MovingAverage#remove(double)} .
+     */
+    @Test
+    public void testRemoveAdvanced() {
+        MovingAverage avg = new MovingAverage();
+        Assert.assertEquals(0.0, avg.getAverage(), PRECISION);
+        Assert.assertEquals(0, avg.getCount());
+
+        avg.add(2);
+        Assert.assertEquals(2, avg.getAverage(), PRECISION);
+        Assert.assertEquals(1, avg.getCount());
+
+        avg.add(2);
+        Assert.assertEquals(2, avg.getAverage(), PRECISION);
+        Assert.assertEquals(2, avg.getCount());
+
+        avg.add(5);
+        Assert.assertEquals(3, avg.getAverage(), PRECISION);
+        Assert.assertEquals(3, avg.getCount());
+
+        // removing a value that was never added, can lead to confusion results
+        avg.remove(7);
+        Assert.assertEquals(1, avg.getAverage(), PRECISION);
+        Assert.assertEquals(2, avg.getCount());
+    }
+
+    /**
+     * Test method for
+     * {@link org.grouplens.lenskit.statistics.MovingAverage#getAverage()} .
+     */
+    @Test
+    public void testGetAverage() {
+        MovingAverage avg = new MovingAverage();
+        Assert.assertEquals(0.0, avg.getAverage(), PRECISION);
+
+        avg.add(5);
+        Assert.assertEquals(5.0, avg.getAverage(), PRECISION);
+
+        avg.add(13);
+        Assert.assertEquals(9, avg.getAverage(), PRECISION);
+    }
+
+    /**
+     * Test method for
+     * {@link org.grouplens.lenskit.statistics.MovingAverage#getCount()} .
+     */
+    @Test
+    public void testGetCount() {
+        MovingAverage avg = new MovingAverage();
+        Assert.assertEquals(0, avg.getCount());
+
+        avg.add(5);
+        Assert.assertEquals(1, avg.getCount());
+
+        avg.add(13);
+        Assert.assertEquals(2, avg.getCount());
+    }
+}
