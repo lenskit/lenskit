87d9ee493ced619ff8eb0028a0534dfc951ba6dd
pikmaihui <unknown>
2012-06-13 13:20:52 -0500

    Add predict-time training process and some features in model to cooperate

--- lenskit-svd/src/main/java/org/grouplens/lenskit/svd/FunkSVDRatingPredictor.java
+++ lenskit-svd/src/main/java/org/grouplens/lenskit/svd/FunkSVDRatingPredictor.java
@@ -18,6 +18,7 @@
  */
 package org.grouplens.lenskit.svd;
 
+import it.unimi.dsi.fastutil.doubles.DoubleArrays;
 import it.unimi.dsi.fastutil.longs.LongIterator;
 import it.unimi.dsi.fastutil.longs.LongSortedSet;
 
@@ -25,7 +26,11 @@ import java.util.Collection;
 
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.time.StopWatch;
 import org.grouplens.lenskit.RatingPredictor;
+import org.grouplens.lenskit.baseline.BaselinePredictor;
+import org.grouplens.lenskit.collections.CollectionUtils;
+import org.grouplens.lenskit.collections.FastCollection;
 import org.grouplens.lenskit.collections.LongSortedArraySet;
 import org.grouplens.lenskit.core.AbstractItemScorer;
 import org.grouplens.lenskit.data.Event;
@@ -33,9 +38,17 @@ import org.grouplens.lenskit.data.UserHistory;
 import org.grouplens.lenskit.data.dao.DataAccessObject;
 import org.grouplens.lenskit.data.event.Rating;
 import org.grouplens.lenskit.data.event.Ratings;
+import org.grouplens.lenskit.data.pref.IndexedPreference;
+import org.grouplens.lenskit.data.snapshot.PreferenceSnapshot;
+import org.grouplens.lenskit.svd.params.IterationCount;
+import org.grouplens.lenskit.svd.params.LearningRate;
+import org.grouplens.lenskit.svd.params.RegularizationTerm;
+import org.grouplens.lenskit.svd.params.TrainingThreshold;
 import org.grouplens.lenskit.transform.clamp.ClampingFunction;
 import org.grouplens.lenskit.vectors.MutableSparseVector;
 import org.grouplens.lenskit.vectors.SparseVector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Do recommendations and predictions based on SVD matrix factorization.
@@ -51,12 +64,38 @@ import org.grouplens.lenskit.vectors.SparseVector;
 public class FunkSVDRatingPredictor extends AbstractItemScorer implements RatingPredictor {
     protected final FunkSVDModel model;
     private DataAccessObject dao;
-
+    
+    
+    //Added
+    
+    private static Logger logger = LoggerFactory.getLogger(FunkSVDRatingPredictor.class);
+    // The default value for feature values - isn't supposed to matter much
+    private static final double DEFAULT_FEATURE_VALUE = 0.1;
+    // Minimum number of epochs to run to train a feature
+    private static final double MIN_EPOCHS = 50;
+    private final int iterationCount;
+    private final double learningRate;
+    private final double trainingThreshold;
+    private final double trainingRegularization;    
+    //End Adding
+    
     @Inject
-    public FunkSVDRatingPredictor(DataAccessObject dao, FunkSVDModel m) {
+    public FunkSVDRatingPredictor(DataAccessObject dao, FunkSVDModel m, 
+    								   @IterationCount int iterCount,
+    	                               @LearningRate double learningRate,
+    	                               @TrainingThreshold double threshold,
+    	                               @RegularizationTerm double gradientDescent) {
         super(dao);
         this.dao = dao;
         model = m;
+        
+        //Added
+        iterationCount = iterCount;
+        this.learningRate = learningRate; 
+        trainingThreshold = threshold;
+        trainingRegularization = gradientDescent;
+        //End Adding
+        
     }
 
     /**
@@ -67,6 +106,150 @@ public class FunkSVDRatingPredictor extends AbstractItemScorer implements Rating
         return model.featureCount;
     }
 
+    //Added
+    private double[] initializeEstimates(long userId, SparseVector ratings) {
+    	double[] estimates = new double[model.numItem];;
+    	MutableSparseVector blpreds = model.baseline.predict(userId, ratings, ratings.keySet());
+    	for (long iid : blpreds.keySet()) {
+    		estimates[model.getItemIndex(iid)] = blpreds.get(iid);
+    	}
+    	return estimates;
+    }
+    
+    private void getUserPrefs(long user, SparseVector ratings, double[] uprefs){    	
+    	
+    	double[] estimates = initializeEstimates(user, ratings);
+    	
+    	if (ratings.isEmpty()){
+    		if (model.getUserIndex(user) < 0){
+    			DoubleArrays.fill(uprefs, 0);
+    		} 
+    		else{
+    			for (int feature = 0; feature < getFeatureCount(); feature++){
+    				uprefs[feature] = model.getUserFeatureValue(model.getUserIndex(user), feature);
+    			}
+    		}
+    	} 
+    	else{
+    		if (model.getUserIndex(user) < 0){
+    			uprefs = model.averUserFeatures;
+    		}
+    		else{
+    			uprefs = PredTimeTraining(user, ratings, uprefs, estimates);
+    		}
+    	}
+    }
+    
+    private double[] PredTimeTraining(long user, SparseVector ratings, 
+    									double[] uprefs, double[] estimates){
+    	int featureCount = getFeatureCount();
+    	double[] trainedUprefs = new double[featureCount];
+    	for (int f = 0; f < featureCount; f++){
+    		predTrainFeature(user, trainedUprefs, uprefs, ratings, estimates, f);
+    	}
+    	return trainedUprefs;
+    }
+    
+    private void predTrainFeature(long user, double[] trainedUprefs, double[] inputUprefs,
+    						SparseVector ratings, double[] estimates, int feature){
+
+        logger.trace("Training feature {}", feature);
+
+        // Fetch and initialize the arrays for this feature
+        final double ufv = inputUprefs[feature];
+        final double[] ifv = model.itemFeatures[feature];
+        final int featureCount = getFeatureCount();
+
+        // We assume that all subsequent features have DEFAULT_FEATURE_VALUE
+        // We can therefore pre-compute the "trailing" prediction value, as it
+        // will be the same for all ratings for this feature.
+        final int rFeatCount = featureCount - feature - 1;
+        final double trailingValue = rFeatCount * DEFAULT_FEATURE_VALUE * DEFAULT_FEATURE_VALUE;
+
+        // Initialize our counters and error tracking
+        double rmse = Double.MAX_VALUE, oldRmse = 0.0;
+        int epoch;
+        StopWatch timer = new StopWatch();
+        timer.start();
+
+        for (epoch = 0; !isDone(epoch, rmse, oldRmse); epoch++) {
+            logger.trace("Running epoch {} of feature {}", epoch, feature);
+            // Save the old RMSE so that we can measure change in error
+            oldRmse = rmse;
+            // Run the iteration and save the error
+            rmse = trainFeatureIteration(user, ratings, ufv, ifv, estimates, trailingValue);
+            logger.trace("Epoch {} had RMSE of {}", epoch, rmse);
+        }
+
+        timer.stop();
+        logger.debug("Finished feature {} in {} epochs (took {}), rmse={}",
+                new Object[]{feature, epoch, timer, rmse});
+
+        // After training this feature, we need to update each rating's cached
+        // value to accommodate it.
+        for (long itemId : ratings.keySet()) {
+            final int idx = model.getItemIndex(itemId);
+            double est = estimates[idx];
+            est = model.clampingFunction.apply(user, itemId,
+                                         est + ufv * ifv[idx]);
+            estimates[idx] = est;
+        }
+    }
+	
+
+    private final double trainFeatureIteration(long user, SparseVector ratings, double ufv, double[] ifvs,
+    												double[] estimates, double trailingValue) {
+        // We'll need to keep track of our sum of squares
+        double ssq = 0;
+        for (long itemId : ratings.keySet()) {
+            ssq += trainRating(user, ufv, ifvs, estimates, trailingValue, itemId, ratings);
+        }
+        // We're done with this feature.  Compute the total error (RMSE)
+        // and head off to the next iteration.
+        return Math.sqrt(ssq / ratings.size());
+    }
+
+    private final double trainRating(long userId, double ufv, double[] ifv, double[] estimates,
+    									double trailingValue, long itemId, SparseVector ratings) {
+    	final int idx = model.getItemIndex(itemId);
+    	final double value = ratings.get(itemId);
+    	// Step 1: get the predicted value (based on preceding features
+    	// and the current feature values)
+    	final double estimateSingleEntry = estimates[idx];
+    	double pred = estimateSingleEntry + ufv * ifv[idx];
+    	pred = model.clampingFunction.apply(userId, itemId, pred);
+
+    	// Step 1b: add the estimate from remaining trailing values
+    	// and apply the result.
+    	pred = model.clampingFunction.apply(userId, itemId, pred + trailingValue);
+
+    	// Step 2: compute the prediction error. We will follow this for
+    	// the gradient descent.
+    	final double err = value - pred;
+
+    	// Step 3: update the feature values.  We'll save the old values first.
+    	final double ouf = ufv;
+    	final double oif = ifv[idx];
+    	// Then we'll update user feature preference
+    	final double udelta = err * oif - trainingRegularization * ouf;
+    	ufv += udelta * learningRate;
+    	// The item feature relevance.remains unchanged
+
+    	// Finally, accumulate the squared error
+    	return err * err;
+    }
+
+    protected final boolean isDone(int epoch, double rmse, double oldRmse) {
+        if (iterationCount > 0) {
+            return epoch >= iterationCount;
+        } else {
+            return epoch >= MIN_EPOCHS && (oldRmse - rmse) < trainingThreshold;
+        }
+    }
+
+    
+    //End Adding
+    
     /**
      * Predict for a user using their preference array and history vector.
      * 
@@ -75,10 +258,16 @@ public class FunkSVDRatingPredictor extends AbstractItemScorer implements Rating
      * @param items The items to predict for.
      * @return The user's predictions.
      */
-    private MutableSparseVector predict(long user, SparseVector ratings, double[] uprefs, Collection<Long> items) {
+    
+    private MutableSparseVector predict(long user, SparseVector ratings,
+    									double[] uprefs, Collection<Long> items) {
         final int nf = model.featureCount;
         final ClampingFunction clamp = model.clampingFunction;
-
+        
+        //Added
+        getUserPrefs(user, ratings, uprefs);
+        //End Adding
+        
         LongSortedSet iset;
         if (items instanceof LongSortedSet) {
             iset = (LongSortedSet) items;
