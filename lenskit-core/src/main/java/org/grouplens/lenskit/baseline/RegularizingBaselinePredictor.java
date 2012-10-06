02f722340a5a0989bb5fac8bfb389495405e91ee
ark <unknown>
2012-10-05 19:05:27 -0500

    init-commit

---
+++ lenskit-core/src/main/java/org/grouplens/lenskit/baseline/RegularizingBaselinePredictor.java
@@ -0,0 +1,14 @@
+package org.grouplens.lenskit.baseline;
+
+import org.grouplens.lenskit.vectors.MutableSparseVector;
+import org.grouplens.lenskit.vectors.SparseVector;
+
+public class RegularizingBaselinePredictor extends AbstractBaselinePredictor {
+
+	@Override
+	public void predict(long user, SparseVector ratings,
+			MutableSparseVector output, boolean predictSet) {
+		
+	}
+
+}
