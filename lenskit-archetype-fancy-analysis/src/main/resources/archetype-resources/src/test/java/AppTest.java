6bef0edb0b7427b01a3099e23181cb7e75f80905
riedl <unknown>
2012-10-31 17:21:15 -0500

    Changes to the pom to use the maven-archetype packaging, with the hope of getting fewer problems.  Change the AppTest.java to no longer require the hamcrest dependencies.

--- lenskit-archetype-fancy-analysis/src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ lenskit-archetype-fancy-analysis/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -1,7 +1,7 @@
 package ${package};
 
-import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 import org.junit.Test;
 
