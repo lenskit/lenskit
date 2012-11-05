95cc80855bbc1491ac80518fa7383afbd20a6d9e
John Riedl <riedl@cs.umn.edu>
2012-11-05 13:17:19 -0600

    Converted the Quantizer tests to use the new API.

--- lenskit-core/src/test/java/org/grouplens/lenskit/transform/quantize/TestValueArrayQuantizer.java
+++ lenskit-core/src/test/java/org/grouplens/lenskit/transform/quantize/TestValueArrayQuantizer.java
@@ -33,18 +33,18 @@ public class TestValueArrayQuantizer {
     public void testFindSingle() {
         Quantizer q = new ValueArrayQuantizer(new double[]{5.0});
         assertThat(q.getCount(), equalTo(1));
-        assertThat(q.getValue(0), equalTo(5.0));
-        assertThat(q.apply(2.5), equalTo(0));
-        assertThat(q.apply(5.0), equalTo(0));
+        assertThat(q.indexToValue(0), equalTo(5.0));
+        assertThat(q.index(2.5), equalTo(0));
+        assertThat(q.index(5.0), equalTo(0));
     }
 
     @Test
     public void testSomeElements() {
         Quantizer q = new ValueArrayQuantizer(new double[]{1.0, 2.0, 3.0, 4.0, 5.0});
         assertThat(q.getCount(), equalTo(5));
-        assertThat(q.getValue(0), equalTo(1.0));
-        assertThat(q.apply(2.5), equalTo(2));
-        assertThat(q.apply(5.0), equalTo(4));
-        assertThat(q.apply(1.73), equalTo(1));
+        assertThat(q.indexToValue(0), equalTo(1.0));
+        assertThat(q.index(2.5), equalTo(2));
+        assertThat(q.index(5.0), equalTo(4));
+        assertThat(q.index(1.73), equalTo(1));
     }
 }
