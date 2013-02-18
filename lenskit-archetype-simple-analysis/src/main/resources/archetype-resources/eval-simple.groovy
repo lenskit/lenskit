7f9ca97b08c9c0a5d0e3a9f32a4c26d074b4fbc4
Michael Ekstrand <ekstrand@cs.umn.edu>
2013-02-18 17:28:50 -0600

    refactor comments to be cleaner

--- lenskit-archetype-simple-analysis/src/main/resources/archetype-resources/eval-simple.groovy
+++ lenskit-archetype-simple-analysis/src/main/resources/archetype-resources/eval-simple.groovy
@@ -35,16 +35,15 @@ trainTest {
     metric NDCGPredictMetric
 
     algorithm("ItemItem") {
-        // use the item-item rating predictor
+        // use the item-item rating predictor with a baseline and normalizer
         bind RatingPredictor to ItemItemRatingPredictor
-        // use item-user personalized means as the baseline
         bind BaselinePredictor to ItemUserMeanPredictor
-        // normalize ratings by subtracting the baseline prior to model building
         bind UserVectorNormalizer to BaselineSubtractingUserVectorNormalizer
-        // use 30 neighbors for prediction
-        set NeighborhoodSize to 30
-        // retain 500 neighbors in the model
+
+        // retain 500 neighbors in the model, use 30 for prediction
         set ModelSize to 500
+        set NeighborhoodSize to 30
+
         // apply some Bayesian smoothing to the mean values
         within(ItemUserMeanPredictor) {
             set Damping to 25.0d
@@ -54,10 +53,12 @@ trainTest {
     algorithm("UserUser") {
         // use the user-user rating predictor
         bind RatingPredictor to UserUserRatingPredictor
-        // use item-user personalized means as the baseline
         bind BaselinePredictor to ItemUserMeanPredictor
-        // normalize ratings by mean-variance (z-score) in predictions
         bind VectorNormalizer to MeanVarianceNormalizer
+
+        // use 30 neighbors for predictions
+        set NeighborhoodSize to 30
+
         // override normalizer within the neighborhood finder
         // this makes it use a different normalizer (subtract user mean) for computing
         // user similarities
@@ -66,8 +67,7 @@ trainTest {
             // override baseline to use user mean
             bind BaselinePredictor to UserMeanPredictor
         }
-        // use 30 neighbors for predictions
-        set NeighborhoodSize to 30
+
         // and apply some Bayesian damping to the baseline
         within(ItemUserMeanPredictor) {
             set Damping to 25.0d
