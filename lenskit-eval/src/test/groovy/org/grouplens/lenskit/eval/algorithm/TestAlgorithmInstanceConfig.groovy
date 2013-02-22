6fda1bafcd56841c4ba0d4c60fb4511532294f3f
Michael Ekstrand <ekstrand@cs.umn.edu>
2013-02-22 15:47:33 -0600

    Improve within/at tests

--- lenskit-eval/src/test/groovy/org/grouplens/lenskit/eval/algorithm/TestAlgorithmInstanceConfig.groovy
+++ lenskit-eval/src/test/groovy/org/grouplens/lenskit/eval/algorithm/TestAlgorithmInstanceConfig.groovy
@@ -30,6 +30,7 @@ import org.grouplens.lenskit.iterative.ThresholdStoppingCondition
 import org.grouplens.lenskit.iterative.params.MinimumIterations
 import org.grouplens.lenskit.iterative.params.StoppingThreshold
 import org.grouplens.lenskit.params.ThresholdValue
+import org.grouplens.lenskit.transform.threshold.AbsoluteThreshold
 import org.grouplens.lenskit.transform.threshold.RealThreshold
 import org.junit.Test
 
@@ -61,9 +62,13 @@ class TestAlgorithmInstanceConfig extends ConfigTestBase {
         def obj = eval {
             algorithm("TestFoo") {
                 root RealThreshold
+                root AbsoluteThreshold
                 root ThresholdStoppingCondition
                 within(RealThreshold) {
                     set ThresholdValue to 0.1d
+                }
+                within(AbsoluteThreshold) {
+                    set ThresholdValue to 0.5d
                 }
                 within(ThresholdStoppingCondition) {
                     set StoppingThreshold to 0.001d
@@ -83,7 +88,10 @@ class TestAlgorithmInstanceConfig extends ConfigTestBase {
             assertThat(stop.minimumIterations, equalTo(42))
             def thresh = rec.get(RealThreshold)
             assertThat(thresh.value,
-                       closeTo(0.1d, 1.0e-6));
+                       closeTo(0.1d, 1.0e-6))
+            def athresh = rec.get(AbsoluteThreshold)
+            assertThat(athresh.value,
+                       closeTo(0.5d, 1.0e-6))
         } finally {
             rec.close()
         }
@@ -94,9 +102,13 @@ class TestAlgorithmInstanceConfig extends ConfigTestBase {
         def obj = eval {
             algorithm("TestFoo") {
                 root RealThreshold
+                root AbsoluteThreshold
                 root ThresholdStoppingCondition
                 at(RealThreshold) {
                     set ThresholdValue to 0.1d
+                }
+                at(AbsoluteThreshold) {
+                    set ThresholdValue to 0.5d
                 }
                 at(ThresholdStoppingCondition) {
                     set StoppingThreshold to 0.001d
@@ -116,7 +128,10 @@ class TestAlgorithmInstanceConfig extends ConfigTestBase {
             assertThat(stop.minimumIterations, equalTo(42))
             def thresh = rec.get(RealThreshold)
             assertThat(thresh.value,
-                       closeTo(0.1d, 1.0e-6));
+                       closeTo(0.1d, 1.0e-6))
+            def athresh = rec.get(AbsoluteThreshold)
+            assertThat(athresh.value,
+                       closeTo(0.5d, 1.0e-6))
         } finally {
             rec.close()
         }
