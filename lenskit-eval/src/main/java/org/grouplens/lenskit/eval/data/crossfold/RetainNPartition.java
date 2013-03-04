11ff5554cfe02327a721563b7c97903dcde0ebfd
Lingfei He <hexxx486@umn.edu>
2013-03-04 15:14:04 -0600

    Add RetainNPartition class to support the retain-N protocol

---
+++ lenskit-eval/src/main/java/org/grouplens/lenskit/eval/data/crossfold/RetainNPartition.java
@@ -0,0 +1,52 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2013 Regents of the University of Minnesota and contributors
+ * Work on LensKit has been funded by the National Science Foundation under
+ * grants IIS 05-34939, 08-08692, 08-12148, and 10-17697.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package org.grouplens.lenskit.eval.data.crossfold;
+
+import static java.lang.Math.min;
+
+import java.util.List;
+
+/**
+ * Partition the last part of list by holding out a fixed number of elements.
+ * Using the number of retain part of items to hold out.
+ *
+ * @param <E>
+ * @author Lingfei He <Lingfei@cs.umn.edu>
+ */
+public class RetainNPartition<E> implements PartitionAlgorithm<E> {
+
+    private int count;
+
+    /**
+     * Create a count partitioner.
+     *
+     * @param n The number of items to put in the train partition.
+     */
+    public RetainNPartition(int n) {
+        count = n;
+    }
+
+    @Override
+    public int partition(List<E> data) {
+        return min(count, data.size());
+    }
+
+}
