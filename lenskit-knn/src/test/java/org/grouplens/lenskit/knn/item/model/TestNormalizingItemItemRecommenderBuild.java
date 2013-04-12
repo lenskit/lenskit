10d73df5386f57e1e8f5777c0e32c54dd97d8a9f
Jack Kolb <kolb0128@umn.edu>
2013-04-12 17:41:03 -0500

    Added lots of missing license headers

--- lenskit-knn/src/test/java/org/grouplens/lenskit/knn/item/model/TestNormalizingItemItemRecommenderBuild.java
+++ lenskit-knn/src/test/java/org/grouplens/lenskit/knn/item/model/TestNormalizingItemItemRecommenderBuild.java
@@ -1,3 +1,23 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2013 Regents of the University of Minnesota and contributors
+ * Work on LensKit has been funded by the National Science Foundation under
+ * grants IIS 05-34939, 08-08692, 08-12148, and 10-17697.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 package org.grouplens.lenskit.knn.item.model;
 
 import org.grouplens.lenskit.*;
@@ -13,10 +33,6 @@ import org.grouplens.lenskit.knn.item.ItemItemGlobalRecommender;
 import org.grouplens.lenskit.knn.item.ItemItemGlobalScorer;
 import org.grouplens.lenskit.knn.item.ItemItemRecommender;
 import org.grouplens.lenskit.knn.item.ItemItemScorer;
-import org.grouplens.lenskit.transform.normalize.DefaultItemVectorNormalizer;
-import org.grouplens.lenskit.transform.normalize.ItemVectorNormalizer;
-import org.grouplens.lenskit.transform.truncate.NoOpTruncator;
-import org.grouplens.lenskit.transform.truncate.VectorTruncator;
 import org.junit.Before;
 import org.junit.Test;
 
