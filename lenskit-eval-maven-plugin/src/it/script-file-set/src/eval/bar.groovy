9b754104e12111dcac60455899a63754b82f21b6
Michael Ekstrand <ekstrand@cs.umn.edu>
2013-04-18 18:00:01 -0500

    add missing license headers

--- lenskit-eval-maven-plugin/src/it/script-file-set/src/eval/bar.groovy
+++ lenskit-eval-maven-plugin/src/it/script-file-set/src/eval/bar.groovy
@@ -1,3 +1,23 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2013 Regents of the University of Minnesota and contributors
+ * Work on LensKit has been funded by the National Science Foundation under
+ * grants IIS 05-34939, 08-08692, 08-12148, and 10-17697.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 def dir = new File(config.analysisDir)
 dir.mkdirs()
 def file = new File(dir, "bar.txt")
