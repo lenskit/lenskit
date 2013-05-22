25738706989c8450434b2856c9edee13144fceb6
Michael Ekstrand <ekstrand@cs.umn.edu>
2013-05-22 16:53:34 -0500

    Add FunkSVD and Slope-One diagrams.

---
+++ lenskit-svd/src/eval/diagram.groovy
@@ -0,0 +1,38 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2013 Regents of the University of Minnesota and contributors
+ * Work on LensKit has been funded by the National Science Foundation under
+ * grants IIS 05-34939, 08-08692, 08-12148, and 10-17697.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+
+import org.grouplens.lenskit.ItemScorer
+import org.grouplens.lenskit.baseline.BaselinePredictor
+import org.grouplens.lenskit.baseline.ItemUserMeanPredictor
+import org.grouplens.lenskit.iterative.IterationCount
+import org.grouplens.lenskit.mf.funksvd.FeatureCount
+import org.grouplens.lenskit.mf.funksvd.FunkSVDItemScorer
+
+dumpGraph {
+    output "${config.analysisDir}/funksvd.dot"
+    algorithm {
+        bind ItemScorer to FunkSVDItemScorer
+        bind BaselinePredictor to ItemUserMeanPredictor
+        set IterationCount to 125
+        set FeatureCount to 25
+    }
+}
