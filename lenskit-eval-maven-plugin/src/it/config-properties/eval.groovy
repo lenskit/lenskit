46eb5ffc2228684410e0eab2b351bd0a11974a1c
Michael Ekstrand <ekstrand@cs.umn.edu>
2013-09-04 11:04:43 -0500

    Fix integration test with lenskit-parent.

--- lenskit-eval-maven-plugin/src/it/config-properties/eval.groovy
+++ lenskit-eval-maven-plugin/src/it/config-properties/eval.groovy
@@ -23,5 +23,5 @@ dir.mkdirs()
 def file = new File(dir, "out.txt")
 file.withPrintWriter {
     // write the data dir to the output file
-    it.print(new File(config.dataDir).getAbsolutePath())
+    it.print(new File(config.dataDir).getCanonicalPath())
 }
--- lenskit-eval-maven-plugin/src/it/config-properties/verify.groovy
+++ lenskit-eval-maven-plugin/src/it/config-properties/verify.groovy
@@ -22,9 +22,9 @@ import static org.hamcrest.MatcherAssert.assertThat
 import static org.hamcrest.Matchers.*
 
 File file = new File("${basedir}/target/analysis", "out.txt")
-File dataPath = new File("${basedir}/data")
+File dataPath = new File(basedir, "data")
 
 assertThat("output file existence", file.isFile(), equalTo(true))
 assertThat("output file text",
            file.getText(),
-           equalTo(dataPath.getAbsolutePath()))
+           equalTo(dataPath.getCanonicalPath()))
