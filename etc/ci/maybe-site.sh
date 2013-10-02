2323cb850602c26bb81f68df3c36339a3c6ce170
Michael Ekstrand <ekstrand@cs.umn.edu>
2013-10-02 13:18:18 -0500

    Set lenskit.web.url for CI site builds

--- etc/ci/maybe-site.sh
+++ etc/ci/maybe-site.sh
@@ -13,7 +13,7 @@ esac
 TARGET=$(echo "$TRAVIS_BRANCH" | sed -e 's@/@-@g')
 
 echo "Building Maven site for $TARGET"
-cmd mvn --batch-mode site site:stage
+cmd mvn --batch-mode site site:stage -Dlenskit.web.url=http://dev.grouplens.org/lenskit/$TARGET
 cmd -d target/staging zip -qr ../lenskit-site.zip *
 if [ -n "$SITE_UPLOAD_URL" ]; then
     cmd python etc/ci/upload-site.py target/lenskit-site.zip \
