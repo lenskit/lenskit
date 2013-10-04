3a6fb1d1f5c11f508a15c9ff1bf2ef32845aa1ba
Michael Ekstrand <ekstrand@cs.umn.edu>
2013-10-04 13:09:26 -0500

    Create zip file in Python for file sanity (#362)
    (cherry picked from commit ce7d080)

--- etc/ci/maybe-site.sh
+++ etc/ci/maybe-site.sh
@@ -13,5 +13,9 @@ esac
 TARGET=$(echo "$TRAVIS_BRANCH" | sed -e 's@/@-@g')
 
 echo "Building Maven site for $TARGET"
-cmd mvn --batch-mode site site:stage
-cmd rsync -r target/staging/ rsync://travis@elehack.net/lenskit-web/lenskit-$TARGET/
+cmd mvn --batch-mode site site:stage -Dlenskit.web.url=http://dev.grouplens.org/lenskit/$TARGET
+if [ -n "$SITE_UPLOAD_URL" ]; then
+    cmd python etc/ci/upload-site.py "$TRAVIS_BRANCH" "$SITE_UPLOAD_URL"
+else
+    echo "No upload URL, skipping upload"
+fi
