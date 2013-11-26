cc5d391ef4a5f1c2c32784446ecdf00b1cd792dd
Michael Ekstrand <ekstrand@cs.umn.edu>
2013-11-26 10:52:54 -0600

    Update least-squares tests for rewriting (#337)

---
+++ lenskit-integration-tests/src/test/java/org/grouplens/lenskit/baseline/LeastSquaresBuildSerializeTest.groovy
@@ -0,0 +1,75 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2013 Regents of the University of Minnesota and contributors
+ * Work on LensKit has been funded by the National Science Foundation under
+ * grants IIS 05-34939, 08-08692, 08-12148, and 10-17697.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package org.grouplens.lenskit.baseline
+
+import org.grouplens.lenskit.ItemScorer
+import org.grouplens.lenskit.RecommenderBuildException
+import org.grouplens.lenskit.config.ConfigHelpers
+import org.grouplens.lenskit.core.LenskitConfiguration
+import org.grouplens.lenskit.core.LenskitRecommender
+import org.grouplens.lenskit.core.LenskitRecommenderEngine
+import org.grouplens.lenskit.core.ModelDisposition
+import org.grouplens.lenskit.data.dao.EventDAO
+import org.grouplens.lenskit.test.ML100KTestSuite
+import org.junit.Test
+
+import static org.hamcrest.Matchers.instanceOf
+import static org.hamcrest.Matchers.notNullValue
+import static org.junit.Assert.assertThat
+
+/**
+ * Do major tests on the item-item recommender.
+ *
+ * @author <a href="http://www.grouplens.org">GroupLens Research</a>
+ */
+public class LeastSquaresBuildSerializeTest extends ML100KTestSuite {
+    @Test
+    public void testBuildAndSerializeModel() throws RecommenderBuildException, IOException {
+        LenskitConfiguration config = ConfigHelpers.load {
+            bind ItemScorer to LeastSquaresItemScorer
+        }
+        LenskitConfiguration daoConfig = ConfigHelpers.load {
+            bind EventDAO to dao
+        }
+
+        LenskitRecommenderEngine engine =
+            LenskitRecommenderEngine.newBuilder()
+                                    .addConfiguration(config)
+                                    .addConfiguration(daoConfig, ModelDisposition.EXCLUDED)
+                                    .build()
+        assertThat(engine, notNullValue())
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream()
+        engine.write(out)
+        byte[] bytes = out.toByteArray()
+
+        ByteArrayInputStream input = new ByteArrayInputStream(bytes)
+        LenskitRecommenderEngine loaded =
+            LenskitRecommenderEngine.newLoader()
+                                    .addConfiguration(daoConfig)
+                                    .load(input)
+        assertThat(loaded, notNullValue())
+
+        LenskitRecommender rec = loaded.createRecommender()
+        assertThat(rec.getItemScorer(),
+                   instanceOf(LeastSquaresItemScorer.class))
+    }
+}
