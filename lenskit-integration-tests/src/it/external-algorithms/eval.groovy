527e7c70d4d00d2b9b92ca5f0927c51a1bfef94e
Michael Ekstrand <ekstrand@cs.umn.edu>
2014-04-23 12:32:24 -0500

    Test the external process item scorer builder
    - Use external process item scorer builder in external-algorithms tests
    - Add Pandas-based test that all 3 item mean scorers produce the same output
    - Make python-pandas available in Travis environment
    - Fix config properties in Gradle scripts

--- lenskit-integration-tests/src/it/external-algorithms/eval.groovy
+++ lenskit-integration-tests/src/it/external-algorithms/eval.groovy
@@ -19,19 +19,48 @@
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
+
 import org.grouplens.lenskit.ItemScorer
-import org.grouplens.lenskit.baseline.BaselineScorer
 import org.grouplens.lenskit.baseline.ItemMeanRatingItemScorer
-import org.grouplens.lenskit.baseline.UserMeanBaseline
-import org.grouplens.lenskit.baseline.UserMeanItemScorer
-import org.grouplens.lenskit.eval.metrics.predict.CoveragePredictMetric
-import org.grouplens.lenskit.eval.metrics.predict.HLUtilityPredictMetric
-import org.grouplens.lenskit.eval.metrics.predict.MAEPredictMetric
-import org.grouplens.lenskit.eval.metrics.predict.NDCGPredictMetric
-import org.grouplens.lenskit.eval.metrics.predict.RMSEPredictMetric
-import org.grouplens.lenskit.knn.item.ItemItemScorer
+import org.grouplens.lenskit.data.dao.EventDAO
+import org.grouplens.lenskit.data.dao.UserDAO
+import org.grouplens.lenskit.eval.data.traintest.QueryData
+import org.grouplens.lenskit.eval.metrics.predict.*
+import org.grouplens.lenskit.external.ExternalProcessItemScorerBuilder
+
+import javax.inject.Inject
+import javax.inject.Provider
 
 def dataDir = config['lenskit.movielens.100k']
+
+/**
+ * Shim class to run item-mean.py to build an ItemScorer.
+ */
+class ExternalItemMeanScorerBuilder implements Provider<ItemScorer> {
+    EventDAO eventDAO
+    UserDAO userDAO
+
+    @Inject
+    public ExternalItemMeanScorerBuilder(EventDAO events, @QueryData UserDAO users) {
+        eventDAO = events
+        userDAO = users
+    }
+
+    @Override
+    ItemScorer get() {
+        def wrk = new File("external-scratch")
+        wrk.mkdirs()
+        def builder = new ExternalProcessItemScorerBuilder()
+        // Note: don't use file names because it will interact badly with crossfolding
+        return builder.setWorkingDir(wrk)
+                      .setExecutable("python")
+                      .addArgument("../item-mean.py")
+                      .addArgument("--for-users")
+                      .addUserFileArgument(userDAO)
+                      .addRatingFileArgument(eventDAO)
+                      .build()
+    }
+}
 
 trainTest {
     dataset crossfold("ML100K") {
@@ -44,12 +73,15 @@ trainTest {
         test 'test.%d.csv'
     }
 
-    externalAlgorithm("item-mean") {
+    externalAlgorithm("FullExternal") {
         command "python", "item-mean.py", "{TRAIN_DATA}", "{TEST_DATA}", "{OUTPUT}"
         workDir config.scriptDir
     }
     algorithm("Baseline") {
         bind ItemScorer to ItemMeanRatingItemScorer
+    }
+    algorithm("External") {
+        bind ItemScorer toProvider ExternalItemMeanScorerBuilder
     }
 
     metric CoveragePredictMetric
