d31703cbea4b853440ed0cb66794382d14c14500
Michael Ekstrand <ekstrand@cs.umn.edu>
2014-04-23 14:34:30 -0500

    Add test for item-item variants producing the same recs

---
+++ lenskit-integration-tests/src/it/item-item-identical/eval.groovy
@@ -0,0 +1,96 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2013 Regents of the University of Minnesota and contributors
+ * Work on LensKit has been funded by the National Science Foundation under
+ * grants IIS 05-34939, 08-08692, 08-12148, and 10-17697.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+
+import org.grouplens.lenskit.ItemScorer
+import org.grouplens.lenskit.RatingPredictor
+import org.grouplens.lenskit.baseline.BaselineScorer
+import org.grouplens.lenskit.baseline.ItemMeanRatingItemScorer
+import org.grouplens.lenskit.core.Transient
+import org.grouplens.lenskit.data.dao.EventDAO
+import org.grouplens.lenskit.data.dao.UserDAO
+import org.grouplens.lenskit.eval.data.traintest.QueryData
+import org.grouplens.lenskit.eval.metrics.predict.*
+import org.grouplens.lenskit.external.ExternalProcessItemScorerBuilder
+import org.grouplens.lenskit.knn.NeighborhoodSize
+import org.grouplens.lenskit.knn.item.ItemItemScorer
+import org.grouplens.lenskit.knn.item.ModelSize
+import org.grouplens.lenskit.knn.item.model.ItemItemBuildContext
+import org.grouplens.lenskit.knn.item.model.ItemItemModel
+import org.grouplens.lenskit.knn.item.model.ItemwiseBuildContextProvider
+import org.grouplens.lenskit.knn.item.model.NormalizingItemItemModelBuilder
+import org.grouplens.lenskit.knn.item.model.StandardVectorTruncatorProvider
+import org.grouplens.lenskit.transform.normalize.MeanCenteringVectorNormalizer
+import org.grouplens.lenskit.transform.normalize.VectorNormalizer
+import org.grouplens.lenskit.transform.truncate.VectorTruncator
+
+import javax.inject.Inject
+import javax.inject.Provider
+
+def dataDir = config['lenskit.movielens.100k']
+
+trainTest {
+    dataset crossfold("ML100K") {
+        source csvfile("$dataDir/u.data") {
+            delimiter "\t"
+        }
+        partitions 5
+        holdout 5
+        train 'train.%d.csv'
+        test 'test.%d.csv'
+    }
+
+    def common = {
+        bind ItemScorer to ItemItemScorer
+        set NeighborhoodSize to 20
+        set ModelSize to 500
+        bind ItemItemBuildContext toProvider ItemwiseBuildContextProvider
+        within (ItemItemBuildContext) {
+            bind VectorNormalizer to MeanCenteringVectorNormalizer
+        }
+        bind (BaselineScorer, ItemScorer) to ItemMeanRatingItemScorer
+        at (RatingPredictor) {
+            // turn off baselines - make sure everything produces the same recs
+            bind (BaselineScorer, ItemScorer) to null
+        }
+    }
+
+    algorithm("Standard") {
+        include common
+    }
+    algorithm("Normalizing") {
+        include common
+        bind ItemItemModel toProvider NormalizingItemItemModelBuilder
+        at (ItemItemModel) {
+            bind VectorTruncator toProvider StandardVectorTruncatorProvider
+        }
+    }
+
+    metric CoveragePredictMetric
+    metric RMSEPredictMetric
+    metric MAEPredictMetric
+    metric NDCGPredictMetric
+    metric HLUtilityPredictMetric
+
+    output 'results.csv'
+    userOutput 'users.csv'
+    predictOutput 'predictions.csv'
+}
---
+++ lenskit-integration-tests/src/it/item-item-identical/verify.py
@@ -0,0 +1,37 @@
+# LensKit, an open source recommender systems toolkit.
+# Copyright 2010-2013 Regents of the University of Minnesota and contributors
+# Work on LensKit has been funded by the National Science Foundation under
+# grants IIS 05-34939, 08-08692, 08-12148, and 10-17697.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 2.1 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+# Verification script to make sure that all 3 algorithms produce the same output.
+# Uses Pandas.
+
+import sys
+import pandas as pd
+
+preds = pd.read_csv('predictions.csv')
+algos = set(preds['Algorithm'])
+preds_wide = preds.pivot_table(rows=['User', 'Item', 'Rating'],
+                               cols='Algorithm',
+                               values='Prediction')
+pred_range = preds_wide.max(1) - preds_wide.min(1)
+bad = preds_wide[pred_range >= 0.001]
+if len(bad) > 0:
+    print >>sys.stderr, "Have %d bad predictions (of %d)" % (len(bad),len(preds_wide))
+    print >>sys.stderr, "Maximum error is %.3f" % (pred_range.max(),)
+    print bad
+    sys.exit(1)
