e689601ed3bbf1f1a8cc25b5dc575cbfa5e38851
Michael Ekstrand <ekstrand@cs.umn.edu>
2014-06-13 15:28:04 -0500

    Update year in copyright header

--- lenskit-eval/src/test/groovy/org/grouplens/lenskit/eval/data/SubsampleConfigTest.groovy
+++ lenskit-eval/src/test/groovy/org/grouplens/lenskit/eval/data/SubsampleConfigTest.groovy
@@ -1,6 +1,6 @@
 /*
  * LensKit, an open source recommender systems toolkit.
- * Copyright 2010-2013 Regents of the University of Minnesota and contributors
+ * Copyright 2010-2014 Regents of the University of Minnesota and contributors
  * Work on LensKit has been funded by the National Science Foundation under
  * grants IIS 05-34939, 08-08692, 08-12148, and 10-17697.
  *
@@ -18,7 +18,6 @@
  * this program; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-
 package org.grouplens.lenskit.eval.data
 
 import com.google.common.io.Files
