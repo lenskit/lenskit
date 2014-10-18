2180759ea7233639a40e68e2d0b278b5f410a436
Michael Ekstrand <ekstrand@txstate.edu>
2014-10-18 15:42:30 -0500

    Add PlusBuilder and a test class (#517)

---
+++ lenskit-core/src/main/java/org/grouplens/lenskit/data/event/PlusBuilder.java
@@ -0,0 +1,82 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2014 LensKit Contributors.  See CONTRIBUTORS.md.
+ * Work on LensKit has been funded by the National Science Foundation under
+ * grants IIS 05-34939, 08-08692, 08-12148, and 10-17697.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package org.grouplens.lenskit.data.event;
+
+import com.google.common.base.Preconditions;
+import org.apache.commons.lang3.builder.Builder;
+
+/**
+ * Builder for {@link Plus} events.
+ *
+ * @since 2.2
+ * @see Plus
+ */
+public class PlusBuilder implements EventBuilder<Plus> {
+    private long userId;
+    private long itemId;
+    private long timestamp = -1;
+    private int count = 1;
+    private boolean hasUserId, hasItemId;
+
+    @Override
+    public void clear() {
+        hasUserId = hasItemId = false;
+        timestamp = -1;
+        count = 1;
+    }
+
+    @Override
+    public PlusBuilder setUserId(long uid) {
+        userId = uid;
+        hasUserId = true;
+        return this;
+    }
+
+    @Override
+    public PlusBuilder setItemId(long iid) {
+        itemId = iid;
+        hasItemId = true;
+        return this;
+    }
+
+    @Override
+    public PlusBuilder setTimestamp(long ts) {
+        timestamp = ts;
+        return this;
+    }
+
+    /**
+     * Set the count associated with the event.
+     * @param ct The count/value.
+     * @return The builder (for chaining).
+     */
+    public PlusBuilder setCount(int ct) {
+        count = ct;
+        return this;
+    }
+
+    @Override
+    public Plus build() {
+        Preconditions.checkArgument(hasUserId, "no user ID set");
+        Preconditions.checkArgument(hasItemId, "no item ID set");
+        return new SimplePlus(userId, itemId, count, timestamp);
+    }
+}
---
+++ lenskit-core/src/test/java/org/grouplens/lenskit/data/event/PlusBuilderTest.java
@@ -0,0 +1,75 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2014 LensKit Contributors.  See CONTRIBUTORS.md.
+ * Work on LensKit has been funded by the National Science Foundation under
+ * grants IIS 05-34939, 08-08692, 08-12148, and 10-17697.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package org.grouplens.lenskit.data.event;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+public class PlusBuilderTest {
+    PlusBuilder builder;
+
+    @Before
+    public void createBuilder() {
+        builder = new PlusBuilder();
+    }
+
+    @Test
+    public void testBasicBuild() {
+        Plus evt = builder.setUserId(42)
+                          .setItemId(39)
+                          .build();
+        assertThat(evt, notNullValue());
+        assertThat(evt.getUserId(), equalTo(42L));
+        assertThat(evt.getItemId(), equalTo(39L));
+        assertThat(evt.getCount(), equalTo(1));
+        assertThat(evt.getTimestamp(), equalTo(-1L));
+    }
+
+    @Test
+    public void testTimestampBuild() {
+        Plus evt = builder.setUserId(42)
+                          .setItemId(39)
+                          .setTimestamp(1000)
+                          .build();
+        assertThat(evt, notNullValue());
+        assertThat(evt.getUserId(), equalTo(42L));
+        assertThat(evt.getItemId(), equalTo(39L));
+        assertThat(evt.getCount(), equalTo(1));
+        assertThat(evt.getTimestamp(), equalTo(1000L));
+    }
+
+    @Test
+    public void testCountBuild() {
+        Plus evt = builder.setUserId(42)
+                          .setItemId(39)
+                          .setCount(50)
+                          .setTimestamp(1000)
+                          .build();
+        assertThat(evt, notNullValue());
+        assertThat(evt.getUserId(), equalTo(42L));
+        assertThat(evt.getItemId(), equalTo(39L));
+        assertThat(evt.getCount(), equalTo(50));
+        assertThat(evt.getTimestamp(), equalTo(1000L));
+    }
+}
