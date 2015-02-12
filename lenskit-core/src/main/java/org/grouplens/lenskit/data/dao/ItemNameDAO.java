98d73ae9410ebf4c00701c7dbcbb914feebbc707
Michael Ekstrand <ekstrand@txstate.edu>
2015-02-12 14:26:15 -0600

    Add ItemNameDAO interface

---
+++ lenskit-core/src/main/java/org/grouplens/lenskit/data/dao/ItemNameDAO.java
@@ -0,0 +1,37 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2014 LensKit Contributors.  See CONTRIBUTORS.md.
+ * Work on LensKit has been funded by the National Science Foundation under
+ * grants IIS 05-34939, 08-08692, 08-12148, and 10-17697.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package org.grouplens.lenskit.data.dao;
+
+import javax.annotation.Nullable;
+
+/**
+ * A DAO interface that provides access to item names.
+ */
+public interface ItemNameDAO {
+    /**
+     * Get the name for an item.
+     * @param item The item ID.
+     * @return A display name for the item, or {@code null} if the item is unknown or has no name.
+     */
+    @Nullable
+    String getItemName(long item);
+}
