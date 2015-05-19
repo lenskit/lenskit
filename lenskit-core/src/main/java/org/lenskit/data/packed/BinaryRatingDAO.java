fa715810c7af171c5c38e97e7ab28dc2e6ac2436
Tajinder Singh <tajinder@txstate.edu>
2015-05-19 13:25:02 -0500

    #663 temporal evaluator

--- lenskit-core/src/main/java/org/lenskit/data/packed/BinaryRatingDAO.java
+++ lenskit-core/src/main/java/org/lenskit/data/packed/BinaryRatingDAO.java
@@ -192,6 +192,10 @@ public class BinaryRatingDAO implements EventDAO, UserEventDAO, ItemEventDAO, Us
         return new BinaryRatingList(header.getFormat(), ratingData, indexes);
     }
 
+    public Long getLimitTimestamp() {
+        return limitTimestamp;
+    }
+    
     @Override
     public ObjectStream<Event> streamEvents() {
         return streamEvents(Event.class);
