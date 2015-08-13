2f6b59b0b0260151bfc59de0151c2eb20ee5d698
Michael Ekstrand <ekstrand@txstate.edu>
2015-08-13 15:16:17 -0500

    Implement and test rating summary

---
+++ lenskit-core/src/main/java/org/lenskit/data/summary/RatingSummary.java
@@ -0,0 +1,99 @@
+package org.lenskit.data.summary;
+
+import org.grouplens.lenskit.core.Shareable;
+import org.lenskit.util.keys.KeyExtractor;
+import org.lenskit.util.keys.KeyedObjectMap;
+
+import java.io.Serializable;
+
+/**
+ * A summary of the ratings data.
+ */
+@Shareable
+public class RatingSummary implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private final double globalMean;
+    private final KeyedObjectMap<ItemSummary> itemSummaries;
+
+    /**
+     * Construct a new rating summary.
+     * @param mean The global mean rating.
+     * @param items The item summaries.
+     */
+    RatingSummary(double mean, KeyedObjectMap<ItemSummary> items) {
+        globalMean = mean;
+        itemSummaries = items;
+    }
+
+    public double getGlobalMean() {
+        return globalMean;
+    }
+
+    /**
+     * Get the mean rating for an item.
+     * @param item The item.
+     * @return The item's mean rating, or {@link Double#NaN} if the item is absent.
+     */
+    public double getItemMean(long item) {
+        ItemSummary sum = itemSummaries.get(item);
+        return sum != null ? sum.getMeanRating() : Double.NaN;
+    }
+
+    /**
+     * Get the item's average offset from global mean.
+     * @param item The item.
+     * @return The item's average offset from the global mean rating, or 0 if the item is missing
+     */
+    public double getItemOffset(long item) {
+        ItemSummary sum = itemSummaries.get(item);
+        if (sum == null) {
+            return 0;
+        } else {
+            return sum.getMeanRating() - globalMean;
+        }
+    }
+
+    /**
+     * Get the number of ratings for the item.
+     */
+    public int getItemRatingCount(long item) {
+        ItemSummary sum = itemSummaries.get(item);
+        return sum != null ? sum.getRatingCount() : 0;
+    }
+
+    static class ItemSummary implements Serializable {
+        private static final long serialVersionUID = 1L;
+
+        private final long id;
+        private final double meanRating;
+        private final int ratingCount;
+
+        public ItemSummary(long item, double mean, int count) {
+            id = item;
+            meanRating = mean;
+            ratingCount = count;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public double getMeanRating() {
+            return meanRating;
+        }
+
+        public int getRatingCount() {
+            return ratingCount;
+        }
+    }
+
+    enum KeyEx implements KeyExtractor<ItemSummary> {
+        INSTANCE {
+            @Override
+            public long getKey(ItemSummary obj) {
+                return obj.getId();
+            }
+        }
+    }
+}
---
+++ lenskit-core/src/test/java/org/lenskit/data/summary/RatingSummaryTest.java
@@ -0,0 +1,40 @@
+package org.lenskit.data.summary;
+
+import org.junit.Test;
+import org.lenskit.util.keys.KeyedObjectMap;
+
+import static org.grouplens.lenskit.util.test.ExtraMatchers.notANumber;
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.*;
+
+public class RatingSummaryTest {
+    @Test
+    public void testEmptySummary() {
+        KeyedObjectMap<RatingSummary.ItemSummary> items =
+                KeyedObjectMap.newBuilder(RatingSummary.KeyEx.INSTANCE)
+                              .build();
+        RatingSummary sum = new RatingSummary(3.5, items);
+        assertThat(sum.getGlobalMean(), equalTo(3.5));
+        assertThat(sum.getItemMean(42), notANumber());
+        assertThat(sum.getItemOffset(42), equalTo(0.0));
+        assertThat(sum.getItemRatingCount(42), equalTo(0));
+    }
+
+    @Test
+    public void testSummaryItem() {
+        RatingSummary.ItemSummary item = new RatingSummary.ItemSummary(37, 3.9, 100);
+        KeyedObjectMap<RatingSummary.ItemSummary> items =
+                KeyedObjectMap.newBuilder(RatingSummary.KeyEx.INSTANCE)
+                              .add(item)
+                              .build();
+        RatingSummary sum = new RatingSummary(3.5, items);
+        assertThat(sum.getGlobalMean(), equalTo(3.5));
+        assertThat(sum.getItemMean(42), notANumber());
+        assertThat(sum.getItemOffset(42), equalTo(0.0));
+        assertThat(sum.getItemRatingCount(42), equalTo(0));
+        assertThat(sum.getItemMean(37), equalTo(3.9));
+        assertThat(sum.getItemOffset(37), closeTo(0.4, 1.0e-6));
+        assertThat(sum.getItemRatingCount(37), equalTo(100));
+    }
+}
