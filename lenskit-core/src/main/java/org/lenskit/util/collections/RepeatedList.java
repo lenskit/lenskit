8c9700806a68d70a40521cc5816e87b9da08348b
Michael Ekstrand <md@ekstrandom.net>
2015-08-14 16:49:50 -0500

    Copy variosu collection utilities into o.l.u.collections

---
+++ lenskit-core/src/main/java/org/lenskit/util/collections/RepeatedList.java
@@ -0,0 +1,58 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2014 LensKit Contributors.  See CONTRIBUTORS.md.
+ * Work on LensKit has been funded by the National Science Foundation under
+ * grants IIS 05-34939, 08-08692, 08-12148, and 10-17697.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package org.lenskit.util.collections;
+
+import java.util.AbstractList;
+
+/**
+ * List consisting of multiple occurrences of one element.
+ *
+ * @param <T> The element type of the list.
+ * @author <a href="http://www.grouplens.org">GroupLens Research</a>
+ */
+class RepeatedList<T> extends AbstractList<T> {
+    private int size;
+    private T object;
+
+    /**
+     * Construct a new repeated list.
+     * @param obj The element.
+     * @param sz length of the list.
+     */
+    public RepeatedList(T obj, int sz) {
+        size = sz;
+        object = obj;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= 0 && index < size) {
+            return object;
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+}
