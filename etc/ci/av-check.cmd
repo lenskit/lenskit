cf2bd7d7ca504fa3135a615a883b3f4bca15f59f
Michael Ekstrand <md@ekstrandom.net>
2015-08-14 15:19:45 -0500

    Simplify AppV build and fix pieces

--- etc/ci/av-check.cmd
+++
@@ -1,3 +0,0 @@
-path C:\Miniconda-x64;C:\Miniconda-x64\Scripts;%PATH%
-activate lenskit
-gradlew.bat check -Pjavadoc.skip=true
--- etc/ci/av-setup.cmd
+++
@@ -1,5 +0,0 @@
-echo on
-path C:\Miniconda-x64;C:\Miniconda-x64\Scripts;%PATH%
-conda env create -f etc\environment.yml
-activate lenskit
-gradlew.bat ciPrep
