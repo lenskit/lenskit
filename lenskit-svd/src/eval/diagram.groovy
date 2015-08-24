1255b49a78815284d9dbd57ff62c5d12e22098c5
Michael Ekstrand <ekstrand@txstate.edu>
2015-08-24 17:12:21 -0500

    Clean up broader tests
    - svd-identical no longer needed (FunkSVDItemScorer is thin wrapper)
    - fix diagram.groovy

--- lenskit-svd/src/eval/diagram.groovy
+++ lenskit-svd/src/eval/diagram.groovy
@@ -20,12 +20,8 @@
  */
 
 
-
-import org.grouplens.lenskit.ItemScorer
-import org.grouplens.lenskit.baseline.BaselineScorer
-import org.grouplens.lenskit.baseline.ItemMeanRatingItemScorer
-import org.grouplens.lenskit.baseline.UserMeanBaseline
-import org.grouplens.lenskit.baseline.UserMeanItemScorer
+import org.lenskit.api.ItemScorer
+import org.lenskit.baseline.*
 import org.grouplens.lenskit.iterative.IterationCount
 import org.lenskit.mf.funksvd.FeatureCount
 import org.lenskit.mf.funksvd.FunkSVDItemScorer
