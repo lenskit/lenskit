276ed46d1bfb339234ef2763bcdc4f4fa2693363
Michael Ekstrand <ekstrand@txstate.edu>
2015-11-03 16:57:55 -0600

    Remove Python installation test

--- lenskit-integration-tests/check-python-deps.py
+++
@@ -1,8 +0,0 @@
-# Check whether we can run with Python dependencies
-
-import sys
-try:
-    import pandas
-except ImportError:
-    print >>sys.stderr, "Module `pandas' not installed"
-    sys.exit(1)
