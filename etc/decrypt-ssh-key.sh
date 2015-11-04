c9d2e5d25ca4c473c493c7323a2ce572c3163f34
Michael Ekstrand <ekstrand@txstate.edu>
2015-11-03 18:21:21 -0600

    decrypt proper file name

--- etc/decrypt-ssh-key.sh
+++ etc/decrypt-ssh-key.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 if [ -n "$encrypted_63db7f4dcbd2_key" ]; then
-    openssl aes-256-cbc -K $encrypted_63db7f4dcbd2_key -iv $encrypted_63db7f4dcbd2_iv -in web-deploy-key.rsa.enc -out $HOME/.ssh/id_rsa -d
+    openssl aes-256-cbc -K $encrypted_63db7f4dcbd2_key -iv $encrypted_63db7f4dcbd2_iv \
+        -in etc/web-deploy-key.rsa.enc -out $HOME/.ssh/id_rsa -d
 fi
