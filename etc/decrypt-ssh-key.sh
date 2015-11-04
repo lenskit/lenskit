aa01f139ad098790fc7248dc8854da56e1bf6ec2
Michael Ekstrand <md@ekstrandom.net>
2015-11-04 09:16:39 -0600

    Install RSA private key with proper permissions

--- etc/decrypt-ssh-key.sh
+++ etc/decrypt-ssh-key.sh
@@ -2,5 +2,6 @@
 
 if [ -n "$encrypted_63db7f4dcbd2_key" ]; then
     openssl aes-256-cbc -K $encrypted_63db7f4dcbd2_key -iv $encrypted_63db7f4dcbd2_iv \
-        -in etc/web-deploy-key.rsa.enc -out $HOME/.ssh/id_rsa -d
+        -in etc/web-deploy-key.rsa.enc -out etc/web-deploy-key.rsa -d
+    install -m 0600 etc/web-deploy-key.rsa "$HOME/.ssh/id_rsa"
 fi
