951de800d3dc71ef640955a72902cb27e91e79b9
Qian Zhao <zhaox331@umn.edu>
2016-02-01 20:57:52 -0600

    added support for EM algorithm and latent variable models

---
+++ lenskit-svd/src/main/java/org/lenskit/mf/hmmsvd/HmmSVDFeatureModel.java
@@ -0,0 +1,22 @@
+package org.grouplens.lenskit.mf.hmmsvd;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.grouplens.lenskit.solver.objective.LatentVariableModel;
+import org.grouplens.lenskit.solver.objective.LearningOracle;
+
+/**
+ * @author <a href="http://www.grouplens.org">GroupLens Research</a>
+ */
+public class HmmSVDFeatureModel implements LatentVariableModel {
+    public HmmSVDFeatureModel() {
+
+    }
+    public expectation() {
+
+    }
+    public maximization() {
+
+    }
+}
