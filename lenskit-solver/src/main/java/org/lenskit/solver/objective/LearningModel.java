c1ac6aba6957e44cb014346f9da52afaf7728ed5
Qian Zhao <zhaox331@umn.edu>
2016-02-02 21:53:42 -0600

    move variable management out of model, simplify model definition and operation, use RealVector and RealMatrix for variables etc.

--- lenskit-solver/src/main/java/org/lenskit/solver/objective/LearningModel.java
+++ lenskit-solver/src/main/java/org/lenskit/solver/objective/LearningModel.java
@@ -5,12 +5,11 @@ import java.io.IOException;
 /**
  * @author <a href="http://www.grouplens.org">GroupLens Research</a>
  */
-public interface LearningModel {
-    public int getNumOfVariables();
-    public int getNumOfAlternation();
-    public LearningOracle getNextOracle() throws IOException;
-    public LearningOracle getNextAlternatingOracle(int alter) throws IOException;
-    public void startNewIteration() throws IOException;
-    public double getVariable(int idx);
-    public void setVariable(int idx, double var);
+public class LearningModel {
+    protected HashMap<String, RealVector> scalarVars;
+    protected HashMap<String, RealMatrix> vectorVars;
+
+    protected void assignVariables();
+    protected LearningOracle getStochasticOracle() throws IOException;
+    protected void startNewIteration() throws IOException;
 }
--- lenskit-solver/src/main/java/org/lenskit/solver/objective/LearningOracle.java
+++ lenskit-solver/src/main/java/org/lenskit/solver/objective/LearningOracle.java
@@ -3,20 +3,39 @@ package org.lenskit.solver.objective;
 import it.unimi.dsi.fastutil.doubles.DoubleArrayList;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 
-public class LearningOracle {
+public class LearningOracle { 
+    private DoubleArrayList scalarGrad;
+    private IntArrayList scalarVarIndexes;
+    private ArrayList<RealVector> vectorGrads;
+    private IntArrayList vectorVarIndexes;
+
     private double objval;
-    private double modelOutput;
-    private double insLabel;
-    private DoubleArrayList variables;
-    private DoubleArrayList gradients;
-    private IntArrayList varIndexes;
 
     public LearningOracle() {
+        scalarVarIndexes = new IntArrayList();
+        scalarVars = new DoubleArrayList();
+        scalarGrad = new DoubleArrayList();
+        vectorVarIndexes = new IntArrayList();
+        vectorVars = new ArrayList<RealVector>();
+        vectorGrads = new ArrayList<RealVector>();
+    }
+
+    public double getObjValue() {
+        return objval;
+    }
+
+    public void setObjValue(double newVal) {
+        objval = newVal;
+    }
+}
+
+public class DiscriminativeOracle extends LearningOracle {
+    private double modelOutput;
+    private double insLabel;
+
+    public DiscriminativeOracle() {
         objval = 0;
         modelOutput = 0;
-        variables = new DoubleArrayList();
-        gradients = new DoubleArrayList();
-        varIndexes = new IntArrayList();
     }
 
     public double getModelOutput() {
@@ -33,49 +52,5 @@ public class LearningOracle {
 
     public void setInstanceLabel(double label) {
         insLabel = label;
-    }
-
-    public double getObjValue() {
-        return objval;
-    }
-
-    public void setObjValue(double newVal) {
-        objval = newVal;
-    }
-
-    public DoubleArrayList getVariables() {
-        return variables;
-    }
-
-    public DoubleArrayList getGradients() {
-        return gradients;
-    }
-
-    public IntArrayList getVarIndexes() {
-        return varIndexes;
-    }
-
-    public void addVariable(double var) {
-        variables.add(var);
-    }
-
-    public void addGradient(double grad) {
-        gradients.add(grad);
-    }
-
-    public void addVarIndex(int varIndex) {
-        varIndexes.add(varIndex);
-    }
-
-    public void setVariable(int index, double var) {
-        variables.set(index, var);
-    }
-
-    public void setGradient(int index, double grad) {
-        gradients.set(index, grad);
-    }
-
-    public void setVarIndex(int index, int varInd) {
-        varIndexes.set(index, varInd);
     }
 }
