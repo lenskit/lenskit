73c8b8bd67323e86225a8609f25dd4d9524c8b33
Qian Zhao <zhaox331@umn.edu>
2016-02-13 20:53:09 -0600

    unite testing for svdfeaturemodel

--- lenskit-svd/src/test/java/org/lenskit/mf/svdfeature/SVDFeatureModelBuildTest.java
+++ lenskit-svd/src/test/java/org/lenskit/mf/svdfeature/SVDFeatureModelBuildTest.java
@@ -15,13 +15,18 @@ import static org.junit.Assert.assertThat;
 public class SVDFeatureModelBuildTest {
     @Test
     public void testModelBuild() throws FileNotFoundException, IOException {
-        String path = "/home/qian/Study/pyml/NoisyNegativeImplicitFeedback/data/svdfea11-clkrat.te";
+        String train = "/home/qian/Study/pyml/NoisyNegativeImplicitFeedback/data/svdfea11-clkrat.tr";
         int numBiases = 38544;
         int numFactors = 38543;
         int dim = 20;
-        SVDFeatureInstanceDAO dao = new SVDFeatureInstanceDAO(new File(path), " ");
+        SVDFeatureInstanceDAO dao = new SVDFeatureInstanceDAO(new File(train), " ");
         LogisticLoss loss = new LogisticLoss();
-        SVDFeatureModelBuilder modelBuilder = new SVDFeatureModelBuilder(numBiases, numFactors, dim, dao, loss);
+        SVDFeatureModelBuilder modelBuilder = new SVDFeatureModelBuilder(numBiases, numFactors,
+                dim, dao, loss);
         SVDFeatureModel model = modelBuilder.build();
+        String modelFile = "/home/qian/Study/pyml/NoisyNegativeImplicitFeedback/data/svdfea11-clkrat.model";
+        ObjectOutputStream fout = new ObjectOutputStream(new FileOutputStream(modelFile));
+        fout.writeObject(model);
+        fout.close();
     }
 }
