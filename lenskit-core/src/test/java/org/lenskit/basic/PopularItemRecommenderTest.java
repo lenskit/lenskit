078d33584cd76b1790657036b384db70b27f119a
Michael Ekstrand <ekstrand@txstate.edu>
2016-02-15 14:58:59 -0600

    Add a Popular Items recommender

---
+++ lenskit-core/src/test/java/org/lenskit/basic/PopularItemRecommenderTest.java
@@ -0,0 +1,76 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2014 LensKit Contributors.  See CONTRIBUTORS.md.
+ * Work on LensKit has been funded by the National Science Foundation under
+ * grants IIS 05-34939, 08-08692, 08-12148, and 10-17697.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package org.lenskit.basic;
+
+import it.unimi.dsi.fastutil.longs.LongSets;
+import org.junit.Before;
+import org.junit.Test;
+import org.lenskit.api.ItemRecommender;
+import org.lenskit.data.dao.EventCollectionDAO;
+import org.lenskit.data.dao.EventDAO;
+import org.lenskit.data.dao.PrefetchingUserEventDAO;
+import org.lenskit.data.ratings.Rating;
+import org.lenskit.data.ratings.RatingSummary;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+public class PopularItemRecommenderTest {
+    EventDAO ratings;
+    RatingSummary summary;
+    ItemRecommender recommender;
+
+    @Before
+    public void setUp() {
+        ratings = EventCollectionDAO.create(Rating.create(42, 1, 3.2),
+                                            Rating.create(39, 1, 2.4),
+                                            Rating.create(42, 2, 2.5));
+        summary = RatingSummary.create(ratings);
+        recommender = new PopularItemRecommender(summary, new PrefetchingUserEventDAO(ratings));
+    }
+
+    @Test
+    public void testNoCandidates() {
+        assertThat(recommender.recommend(42, 2, LongSets.EMPTY_SET, LongSets.EMPTY_SET),
+                   hasSize(0));
+    }
+
+    @Test
+    public void testDefaultAll() {
+        // unseen user, default, recommend all the items
+        assertThat(recommender.recommend(71),
+                   contains(1L, 2L));
+    }
+
+    @Test
+    public void testDefaultExcludeAll() {
+        // user rated everything user, default, recommend no items
+        assertThat(recommender.recommend(42),
+                   hasSize(0));
+    }
+
+    @Test
+    public void testDefaultExcludeOne() {
+        // user rated everything user, default, recommend no items
+        assertThat(recommender.recommend(39),
+                   contains(2L));
+    }
+}
