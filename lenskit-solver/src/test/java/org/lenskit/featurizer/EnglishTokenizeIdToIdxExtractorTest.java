4164222a33cc7098886604edd6e1e8581940f5b8
Qian Zhao <zhaox331@umn.edu>
2016-04-25 11:12:20 -0500

    started adding unit test cases

---
+++ lenskit-solver/src/test/java/org/lenskit/featurizer/EnglishTokenizeIdToIdxExtractorTest.java
@@ -0,0 +1,4 @@
+package org.lenskit.featurizer;
+
+public class EnglishTokenizeIdToIdxExtractorTest {
+}
---
+++ lenskit-solver/src/test/java/org/lenskit/space/SynchronizedVariableSpaceTest.java
@@ -0,0 +1,4 @@
+package org.lenskit.space;
+
+public class SynchronizedVariableSpaceTest {
+}
