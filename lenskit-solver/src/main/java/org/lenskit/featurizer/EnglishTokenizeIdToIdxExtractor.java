aaae10b8c833d4919df8eceb4e35ef40bbbd2c59
Qian Zhao <zhaox331@umn.edu>
2016-04-25 09:37:14 -0500

    added EnglishTokenize extractor

---
+++ lenskit-solver/src/main/java/org/lenskit/featurizer/EnglishTokenizeIdToIdxExtractor.java
@@ -0,0 +1,47 @@
+package org.lenskit.featurize;
+
+import org.lenskit.space.IndexSpace;
+import org.lenskit.space.SynchronizedIndexSpace;
+import org.lenskit.space.VariableSpace;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EnglishTokenizeIdToIdxExtractor implements FeatureExtractor {
+    private final String indexName;
+    private final String attrName;
+    private final String feaName;
+
+    public EnglishTokenizeIdToIdxExtractor(String indexName,
+                                           String attrName,
+                                           String feaName) {
+        this.indexName = indexName;
+        this.attrName = attrName;
+        this.feaName = feaName;
+    }
+
+    public Map<String, List<Feature>> extract(Entity entity, boolean update,
+                                              IndexSpace indexSpace) {
+        List<Feature> features = new ArrayList<>();
+        List<String> attrs = entity.getCatAttr(attrName);
+        for (String attr : attrs) {
+            String[] tokens = attr.split(" ");
+            for (String token : tokens) {
+                String key = attrName + ":" + token;
+                if (indexSpace.containsStringKey(indexName, key)) {
+                    Feature feature = new Feature(indexSpace.getIndexForStringKey(indexName, key), 1.0);
+                    features.add(feature);
+                } else if (update) {
+                    int index = indexSpace.setStringKey(indexName, key);
+                    Feature feature = new Feature(index, 1.0);
+                    features.add(feature);
+                }
+            }
+        }
+        Map<String, List<Feature>> feaMap = new HashMap<>();
+        feaMap.put(feaName, features);
+        return feaMap;
+    }
+}
