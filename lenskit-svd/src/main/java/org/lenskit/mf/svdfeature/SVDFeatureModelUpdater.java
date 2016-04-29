278fbc05ba9ab22f3b9d749ef5e1927426b720c3
Qian Zhao <zhaox331@umn.edu>
2016-04-29 00:14:56 -0500

    minor error fixes for svdfeature updater and entity list

--- lenskit-svd/src/main/java/org/lenskit/mf/svdfeature/SVDFeatureModelUpdater.java
+++ lenskit-svd/src/main/java/org/lenskit/mf/svdfeature/SVDFeatureModelUpdater.java
@@ -12,7 +12,7 @@ public class SVDFeatureModelUpdater {
         this.method = method;
     }
 
-    public void update(SVDFeatureEntityList learningData) {
+    public void update(LearningData learningData) {
         method.update(model, learningData);
     }
 }
