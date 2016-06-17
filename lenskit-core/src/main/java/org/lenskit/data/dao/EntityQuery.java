26b42c736821f41b8b4536dba9f6285baffeb95c
Michael Ekstrand <md@ekstrandom.net>
2016-06-17 10:27:15 -0500

    Create entity query API
    This creates the entity query data structure, its builder, and tests.

---
+++ lenskit-core/src/main/java/org/lenskit/data/dao/EntityQuery.java
@@ -0,0 +1,117 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2014 LensKit Contributors.  See CONTRIBUTORS.md.
+ * Work on LensKit has been funded by the National Science Foundation under
+ * grants IIS 05-34939, 08-08692, 08-12148, and 10-17697.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package org.lenskit.data.dao;
+
+import com.google.common.base.Predicate;
+import org.lenskit.data.entities.Attribute;
+import org.lenskit.data.entities.Entity;
+import org.lenskit.data.entities.EntityType;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+/**
+ * Representation of a query.
+ * @param <E> The entity type.
+ * @see #newBuilder()
+ */
+public class EntityQuery<E extends Entity> implements Predicate<Entity> {
+    private EntityType entityType;
+    private List<Attribute<?>> filterFields;
+    private List<SortKey> sortKeys;
+    private Class<E> viewType;
+
+    EntityQuery(EntityType et, List<Attribute<?>> filt, List<SortKey> sort,
+                Class<E> view) {
+        entityType = et;
+        filterFields = filt;
+        sortKeys = sort;
+        viewType = view;
+    }
+
+    /**
+     * Construct a new data query builder.
+     * @return The query builder.
+     */
+    public static EntityQueryBuilder newBuilder() {
+        return new EntityQueryBuilder();
+    }
+
+    /**
+     * Get the entity type to return.
+     * @return The type of entities to return.
+     */
+    public EntityType getEntityType() {
+        return entityType;
+    }
+
+    /**
+     * Get the field filters. The results should only include entities matching *all* queries.
+     * @return A map of attributes to objects to filter.
+     */
+    public List<Attribute<?>> getFilterFields() {
+        return filterFields;
+    }
+
+    /**
+     * Get the sort keys.
+     * @return The list of keys to sort by.
+     */
+    public List<SortKey> getSortKeys() {
+        return sortKeys;
+    }
+
+    /**
+     * Get the type to view results as.
+     * @return The type to view results as.
+     */
+    public Class<E> getViewType() {
+        return viewType;
+    }
+
+    /**
+     * Call {@link #matches(Entity)}.
+     * @param entity The entity to match.
+     * @return `true` if the entity matches the query
+     */
+    @Override
+    public boolean apply(@Nullable Entity entity) {
+        return entity != null && matches(entity);
+    }
+
+    /**
+     * Determine whether this query matches the specified entity.
+     * @param entity The entity to test.
+     * @return The entity.
+     */
+    public boolean matches(Entity entity) {
+        if (!entity.getType().equals(entityType)) {
+            return false;
+        }
+        for (Attribute<?> attr: filterFields) {
+            Object val = entity.maybeGet(attr.getTypedName());
+            if (!attr.getValue().equals(val)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ lenskit-core/src/test/java/org/lenskit/data/dao/EntityQueryTest.java
@@ -0,0 +1,112 @@
+/*
+ * LensKit, an open source recommender systems toolkit.
+ * Copyright 2010-2014 LensKit Contributors.  See CONTRIBUTORS.md.
+ * Work on LensKit has been funded by the National Science Foundation under
+ * grants IIS 05-34939, 08-08692, 08-12148, and 10-17697.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package org.lenskit.data.dao;
+
+import org.hamcrest.Matcher;
+import org.junit.Test;
+import org.lenskit.data.entities.*;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+public class EntityQueryTest {
+    @Test
+    public void testUniversalQuery() {
+        EntityQuery<Entity> q = EntityQuery.newBuilder().setEntityType(CommonTypes.ITEM).build();
+        assertThat(q.getEntityType(), equalTo(CommonTypes.ITEM));
+        assertThat(q.getFilterFields(), hasSize(0));
+        assertThat(q.getSortKeys(), hasSize(0));
+        assertThat(q.getViewType(), equalTo(Entity.class));
+    }
+
+    @Test
+    public void testSingleAttrFilter() {
+        EntityQuery<Entity> q = EntityQuery.newBuilder()
+                                           .setEntityType(CommonTypes.RATING)
+                                           .addEqualsCondition(CommonAttributes.ITEM_ID, 42L)
+                                           .build();
+        assertThat(q.getEntityType(), equalTo(CommonTypes.RATING));
+        assertThat(q.getFilterFields(),
+                   (Matcher) contains(AttributeValue.create(CommonAttributes.ITEM_ID, 42L)));
+        assertThat(q.getSortKeys(), hasSize(0));
+        assertThat(q.getViewType(), equalTo(Entity.class));
+    }
+
+    @Test
+    public void testTwoAttrFilter() {
+        EntityQuery<Entity> q = EntityQuery.newBuilder()
+                                           .setEntityType(CommonTypes.RATING)
+                                           .addEqualsCondition(CommonAttributes.ITEM_ID, 42L)
+                                           .addEqualsCondition(CommonAttributes.USER_ID, 29L)
+                                           .build();
+        assertThat(q.getEntityType(), equalTo(CommonTypes.RATING));
+        assertThat(q.getFilterFields(),
+                   (Matcher) containsInAnyOrder(AttributeValue.create(CommonAttributes.ITEM_ID, 42L),
+                                                AttributeValue.create(CommonAttributes.USER_ID, 29L)));
+        assertThat(q.getSortKeys(), hasSize(0));
+        assertThat(q.getViewType(), equalTo(Entity.class));
+    }
+
+    @Test
+    public void testSortKey() {
+        EntityQuery<Entity> q = EntityQuery.newBuilder()
+                                           .setEntityType(CommonTypes.RATING)
+                                           .addSortKey(CommonAttributes.ITEM_ID)
+                                           .build();
+        assertThat(q.getEntityType(), equalTo(CommonTypes.RATING));
+        assertThat(q.getFilterFields(), hasSize(0));
+        assertThat(q.getSortKeys(),
+                   contains(new SortKey(CommonAttributes.ITEM_ID, SortOrder.ASCENDING)));
+        assertThat(q.getViewType(),
+                   equalTo(Entity.class));
+    }
+
+    @Test
+    public void testUniversalMatch() {
+        EntityQuery<Entity> q = EntityQuery.newBuilder()
+                                           .setEntityType(CommonTypes.ITEM)
+                                           .build();
+        assertThat(q.matches(Entities.create(CommonTypes.ITEM, 42L)),
+                   equalTo(true));
+        assertThat(q.matches(Entities.create(CommonTypes.USER, 42L)),
+                   equalTo(false));
+    }
+
+    @Test
+    public void testAttributeMatch() {
+        EntityQuery<Entity> q = EntityQuery.newBuilder()
+                                           .setEntityType(CommonTypes.RATING)
+                                           .addEqualsCondition(CommonAttributes.USER_ID, 42L)
+                                           .build();
+        assertThat(q.matches(Entities.create(CommonTypes.RATING, 42L)),
+                   equalTo(false));
+        assertThat(q.matches(Entities.create(CommonTypes.USER, 42L)),
+                   equalTo(false));
+        assertThat(q.matches(Entities.newBuilder(CommonTypes.RATING, 42L)
+                                     .setAttribute(CommonAttributes.USER_ID, 42L)
+                                     .build()),
+                   equalTo(true));
+        assertThat(q.matches(Entities.newBuilder(CommonTypes.RATING, 42L)
+                                     .setAttribute(CommonAttributes.USER_ID, 78L)
+                                     .build()),
+                   equalTo(false));
+    }
+}
