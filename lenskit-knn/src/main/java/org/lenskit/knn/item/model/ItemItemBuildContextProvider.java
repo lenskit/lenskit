fe60a9f726bb5efad0106e8017a9b352848b4b72
Michael Ekstrand <md@ekstrandom.net>
2016-07-06 20:48:05 -0500

    Make item-item context provider use streaming vectors

--- lenskit-knn/src/main/java/org/lenskit/knn/item/model/ItemItemBuildContextProvider.java
+++ lenskit-knn/src/main/java/org/lenskit/knn/item/model/ItemItemBuildContextProvider.java
@@ -20,7 +20,10 @@
  */
 package org.lenskit.knn.item.model;
 
-import it.unimi.dsi.fastutil.longs.*;
+import it.unimi.dsi.fastutil.longs.Long2DoubleMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.longs.LongSortedSet;
 import org.grouplens.lenskit.scored.ScoredIdListBuilder;
 import org.grouplens.lenskit.scored.ScoredIds;
 import org.grouplens.lenskit.transform.normalize.UserVectorNormalizer;
@@ -28,11 +31,11 @@ import org.grouplens.lenskit.vectors.ImmutableSparseVector;
 import org.grouplens.lenskit.vectors.MutableSparseVector;
 import org.grouplens.lenskit.vectors.SparseVector;
 import org.grouplens.lenskit.vectors.VectorEntry;
-import org.lenskit.data.dao.DataAccessObject;
-import org.lenskit.data.entities.CommonTypes;
 import org.lenskit.data.ratings.RatingVectorDAO;
 import org.lenskit.inject.Transient;
+import org.lenskit.util.IdBox;
 import org.lenskit.util.collections.LongUtils;
+import org.lenskit.util.io.ObjectStream;
 import org.lenskit.util.keys.SortedKeyIndex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,22 +52,18 @@ public class ItemItemBuildContextProvider implements Provider<ItemItemBuildConte
 
     private static final Logger logger = LoggerFactory.getLogger(ItemItemBuildContextProvider.class);
 
-    private final DataAccessObject dao;
     private final RatingVectorDAO rvDAO;
     private final UserVectorNormalizer normalizer;
 
     /**
      * Construct an item-item build context provider.
      *
-     * @param dao The data access object.
      * @param rvd The rating vector DAO.
      * @param normalizer The user vector normalizer.
      */
     @Inject
-    public ItemItemBuildContextProvider(@Transient DataAccessObject dao,
-                                        @Transient RatingVectorDAO rvd,
+    public ItemItemBuildContextProvider(@Transient RatingVectorDAO rvd,
                                         @Transient UserVectorNormalizer normalizer) {
-        this.dao = dao;
         rvDAO = rvd;
         this.normalizer = normalizer;
     }
@@ -114,26 +113,28 @@ public class ItemItemBuildContextProvider implements Provider<ItemItemBuildConte
     private void buildItemRatings(Long2ObjectMap<ScoredIdListBuilder> itemRatings,
                                   Long2ObjectMap<LongSortedSet> userItems) {
         // initialize the transposed array to collect item vector data
-        LongSet users = dao.getEntityIds(CommonTypes.USER);
-        for (long uid: users) {
-            Long2DoubleMap ratings = rvDAO.userRatingVector(uid);
-            SparseVector summary = ImmutableSparseVector.create(ratings);
-            MutableSparseVector normed = summary.mutableCopy();
-            normalizer.normalize(uid, summary, normed);
+        try (ObjectStream<IdBox<Long2DoubleMap>> stream = rvDAO.streamUsers()) {
+            for (IdBox<Long2DoubleMap> user : stream) {
+                long uid = user.getId();
+                Long2DoubleMap ratings = user.getValue();
+                SparseVector summary = ImmutableSparseVector.create(ratings);
+                MutableSparseVector normed = summary.mutableCopy();
+                normalizer.normalize(uid, summary, normed);
 
-            for (VectorEntry rating : normed) {
-                final long item = rating.getKey();
-                // get the item's rating accumulator
-                ScoredIdListBuilder ivect = itemRatings.get(item);
-                if (ivect == null) {
-                    ivect = ScoredIds.newListBuilder(100);
-                    itemRatings.put(item, ivect);
+                for (VectorEntry rating : normed) {
+                    final long item = rating.getKey();
+                    // get the item's rating accumulator
+                    ScoredIdListBuilder ivect = itemRatings.get(item);
+                    if (ivect == null) {
+                        ivect = ScoredIds.newListBuilder(100);
+                        itemRatings.put(item, ivect);
+                    }
+                    ivect.add(uid, rating.getValue());
                 }
-                ivect.add(uid, rating.getValue());
-            }
 
-            // get the item's candidate set
-            userItems.put(uid, LongUtils.packedSet(summary.keySet()));
+                // get the item's candidate set
+                userItems.put(uid, LongUtils.packedSet(summary.keySet()));
+            }
         }
     }
 }
