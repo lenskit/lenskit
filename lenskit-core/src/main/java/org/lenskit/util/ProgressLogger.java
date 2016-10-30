9e50030f46c04167a6b87b9febc5ddf9ee0e903a
Michael Ekstrand <md@ekstrandom.net>
2016-10-30 13:56:48 -0600

    add time-based reporting to progress logger

--- lenskit-core/src/main/java/org/lenskit/util/ProgressLogger.java
+++ lenskit-core/src/main/java/org/lenskit/util/ProgressLogger.java
@@ -35,6 +35,7 @@ public class ProgressLogger {
     private String label = "unspecified";
     private int total = -1;
     private int period = 100;
+    private long timePeriod = TimeUnit.MICROSECONDS.convert(30, TimeUnit.SECONDS);
     private int prevN;
     private long prevMicros;
     private int ndone = 0;
@@ -85,6 +86,16 @@ public class ProgressLogger {
     }
 
     /**
+     * Set a time period for reporting progress.
+     * @param secs The time period for reporting progress.
+     * @return The logger (for chaining).
+     */
+    public ProgressLogger setTimePeriod(long secs) {
+        timePeriod = TimeUnit.MICROSECONDS.convert(secs, TimeUnit.SECONDS);
+        return this;
+    }
+
+    /**
      * Set the window to use for smoothing averages.
      * @param w The window size, in number of periods.
      * @return The progress logger (for chaining).
@@ -114,6 +125,12 @@ public class ProgressLogger {
         }
         if (ndone % period == 0) {
             logProgress();
+        } else {
+            // log every 30 seconds
+            long micros = timer.elapsed(TimeUnit.MICROSECONDS);
+            if (micros - prevMicros > timePeriod) {
+                logProgress(micros);
+            }
         }
     }
 
@@ -122,6 +139,10 @@ public class ProgressLogger {
      */
     public void logProgress() {
         long micros = timer.elapsed(TimeUnit.MICROSECONDS);
+        logProgress(micros);
+    }
+
+    private void logProgress(long micros) {
         long elapsed = micros - prevMicros;
         prevMicros = micros;
         double time = elapsed * 0.000001;
