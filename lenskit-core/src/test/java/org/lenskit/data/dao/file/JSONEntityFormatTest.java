59457cc2060d6b7593fc142a8f73b73120fe2586
Michael Ekstrand <michaelekstrand@boisestate.edu>
2017-02-06 17:38:01 -0700

    Support configuring fields for JSON entity formats

--- lenskit-core/src/test/java/org/lenskit/data/dao/file/JSONEntityFormatTest.java
+++ lenskit-core/src/test/java/org/lenskit/data/dao/file/JSONEntityFormatTest.java
@@ -20,14 +20,19 @@
  */
 package org.lenskit.data.dao.file;
 
-import com.google.common.collect.Lists;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import org.grouplens.grapht.util.ClassLoaders;
 import org.junit.Test;
+import org.lenskit.data.entities.CommonAttributes;
 import org.lenskit.data.entities.CommonTypes;
 import org.lenskit.data.entities.Entities;
 import org.lenskit.data.entities.Entity;
 import org.lenskit.data.ratings.Rating;
 import org.lenskit.data.ratings.RatingBuilder;
 
+import java.io.IOException;
 import java.util.Collections;
 
 import static org.hamcrest.Matchers.*;
@@ -63,5 +68,53 @@ public class JSONEntityFormatTest {
         assertThat(r.getUserId(), equalTo(42L));
         assertThat(r.getItemId(), equalTo(20L));
         assertThat(r.getValue(), equalTo(3.5));
+    }
+
+    @Test
+    public void testThingFields() {
+        JSONEntityFormat fmt = new JSONEntityFormat();
+        fmt.setEntityType(CommonTypes.ITEM);
+        fmt.addAttribute(CommonAttributes.ENTITY_ID);
+        fmt.addAttribute("title", CommonAttributes.NAME);
+
+        LineEntityParser lep = fmt.makeParser(Collections.EMPTY_LIST);
+        Entity res = lep.parse("{\"id\": 204, \"title\": \"hamster\", \"extra\": \"wumpus\"}");
+        assertThat(res, notNullValue());
+        assertThat(res.getId(), equalTo(204L));
+        assertThat(res.get(CommonAttributes.NAME), equalTo("hamster"));
+        assertThat(res.hasAttribute("extra"), equalTo(false));
+    }
+
+    @Test
+    public void testConfigureReader() throws IOException {
+        ObjectReader reader = new ObjectMapper().reader();
+        JsonNode json = reader.readTree("{\"entity_type\": \"item\"}");
+        EntityFormat fmt = JSONEntityFormat.fromJSON(null, ClassLoaders.inferDefault(), json);
+        assertThat(fmt.getEntityType(), equalTo(CommonTypes.ITEM));
+
+        LineEntityParser lep = fmt.makeParser(Collections.EMPTY_LIST);
+        Entity res = lep.parse("{\"id\": 204, \"name\": \"hamster\", \"extra\": \"wumpus\"}");
+        assertThat(res, notNullValue());
+        assertThat(res.getId(), equalTo(204L));
+        assertThat(res.get(CommonAttributes.NAME), equalTo("hamster"));
+        assertThat(res.get("extra"), equalTo("wumpus"));
+    }
+
+    @Test
+    public void testConfigureReaderFields() throws IOException {
+        ObjectReader reader = new ObjectMapper().reader();
+        JsonNode json = reader.readTree("{\"entity_type\": \"item\", \"attributes\": {\"id\": \"long\", \"title\": {\"name\": \"name\", \"type\": \"string\"}}}");
+        JSONEntityFormat fmt = JSONEntityFormat.fromJSON(null, ClassLoaders.inferDefault(), json);
+        assertThat(fmt.getEntityType(), equalTo(CommonTypes.ITEM));
+        assertThat(fmt.getAttributes(), hasEntry("id", CommonAttributes.ENTITY_ID));
+        assertThat(fmt.getAttributes(), hasEntry("title", CommonAttributes.NAME));
+        assertThat(fmt.getAttributes().size(), equalTo(2));
+
+        LineEntityParser lep = fmt.makeParser(Collections.EMPTY_LIST);
+        Entity res = lep.parse("{\"id\": 204, \"title\": \"hamster\", \"extra\": \"wumpus\"}");
+        assertThat(res, notNullValue());
+        assertThat(res.getId(), equalTo(204L));
+        assertThat(res.get(CommonAttributes.NAME), equalTo("hamster"));
+        assertThat(res.hasAttribute("extra"), equalTo(false));
     }
 }
