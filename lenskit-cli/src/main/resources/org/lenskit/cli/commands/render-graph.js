99a192c5f8b9d30f1a37c41be44fdbce6124c88f
Michael Ekstrand <michaelekstrand@boisestate.edu>
2017-02-10 15:11:49 -0700

    Add missing render-graph.js (#318)

---
+++ lenskit-cli/src/main/resources/org/lenskit/cli/commands/render-graph.js
@@ -0,0 +1,10 @@
+load('classpath:META-INF/resources/webjars/viz.js/1.5.1/viz.js');
+
+var svg = Viz(dotSrc, {
+    format: "svg",
+    engine: "dot"
+});
+
+var writer = new java.io.FileWriter(outFile);
+writer.write(svg);
+writer.close();
