849e6e67de82ad881bab2b806fd950a790428f70
Michael Ekstrand <michaelekstrand@boisestate.edu>
2017-02-09 18:14:38 -0700

    Add inject annotation to MeanVarNorm

--- lenskit-core/src/main/java/org/lenskit/transform/normalize/MeanVarianceNormalizer.java
+++ lenskit-core/src/main/java/org/lenskit/transform/normalize/MeanVarianceNormalizer.java
@@ -38,6 +38,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
+import javax.inject.Inject;
 import java.io.Serializable;
 
 /**
@@ -61,6 +62,7 @@ public class MeanVarianceNormalizer extends AbstractVectorNormalizer implements 
     /**
      * Initializes basic normalizer with no damping.
      */
+    @Inject
     public MeanVarianceNormalizer() {
         this(0, 0);
     }
