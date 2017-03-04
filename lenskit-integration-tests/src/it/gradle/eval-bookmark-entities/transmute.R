d4958cc013ac7f242b6d23f54042f703b2b63475
Michael Ekstrand <michaelekstrand@boisestate.edu>
2017-03-04 16:17:53 -0700

    Implement integration test for non-rating entities
    This adds an integration test for #993 (working on #977).

---
+++ lenskit-integration-tests/src/it/gradle/eval-bookmark-entities/transmute.R
@@ -0,0 +1,17 @@
+args = commandArgs(TRUE)
+rating_file = args[0]
+
+ratings = read.delim(rating_file, header=FALSE, col.names=c("user", "item", "rating", "timestamp"))
+n = nrow(ratings)
+purchases = data.frame(id=1:n, user=ratings$user, item=ratings$item, timestamp=ratings$timestamp)
+
+
+test1 = vector("logical", n)
+test2 = vector("logical", n)
+test1[,sample(1:n, 5000)] = TRUE
+test2[,sample(1:n, 5000)] = TRUE
+
+write.table(purchases[test1], file="bookmark-test-1.csv", sep=",", row.names=FALSE)
+write.table(purchases[test2], file="bookmark-test-2.csv", sep=",", row.names=FALSE)
+write.table(purchases[!test1], file="bookmark-train-1.csv", sep=",", row.names=FALSE)
+write.table(purchases[!test2], file="bookmark-train-2.csv", sep=",", row.names=FALSE)
