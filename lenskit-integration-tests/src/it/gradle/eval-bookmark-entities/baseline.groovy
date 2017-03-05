9e5ee5e6caa39fce01359cf825c006bd0c4fa1b5
Michael Ekstrand <michaelekstrand@boisestate.edu>
2017-03-04 18:18:58 -0700

    Improve integration tests for #977

--- lenskit-integration-tests/src/it/gradle/eval-bookmark-entities/baseline.groovy
+++ lenskit-integration-tests/src/it/gradle/eval-bookmark-entities/baseline.groovy
@@ -1,7 +1,7 @@
 import org.lenskit.api.ItemScorer
-import org.lenskit.baseline.ItemMeanRatingItemScorer
-import org.lenskit.baseline.UserMeanBaseline
-import org.lenskit.baseline.UserMeanItemScorer
+import org.lenskit.basic.PopularityRankItemScorer
+import org.lenskit.data.entities.EntityType
+import org.lenskit.data.ratings.InteractionEntityType
 
-bind ItemScorer to UserMeanItemScorer
-bind (UserMeanBaseline, ItemScorer) to ItemMeanRatingItemScorer
+bind ItemScorer to PopularityRankItemScorer
+set InteractionEntityType to EntityType.forName("bookmark")
