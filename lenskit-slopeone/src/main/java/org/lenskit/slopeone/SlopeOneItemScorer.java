335034a030b3e6a6ac4d5f5d62d7313a4769dbf0
Michael Ekstrand <md@ekstrandom.net>
2017-03-08 22:24:54 -0700

    Remove vector from slope-one scorers

--- lenskit-slopeone/src/main/java/org/lenskit/slopeone/SlopeOneItemScorer.java
+++ lenskit-slopeone/src/main/java/org/lenskit/slopeone/SlopeOneItemScorer.java
@@ -23,9 +23,6 @@ package org.lenskit.slopeone;
 import it.unimi.dsi.fastutil.longs.Long2DoubleMap;
 import it.unimi.dsi.fastutil.longs.LongIterator;
 import it.unimi.dsi.fastutil.longs.LongIterators;
-import org.grouplens.lenskit.vectors.ImmutableSparseVector;
-import org.grouplens.lenskit.vectors.SparseVector;
-import org.grouplens.lenskit.vectors.VectorEntry;
 import org.lenskit.api.ItemScorer;
 import org.lenskit.api.Result;
 import org.lenskit.api.ResultMap;
@@ -33,6 +30,7 @@ import org.lenskit.basic.AbstractItemScorer;
 import org.lenskit.data.ratings.PreferenceDomain;
 import org.lenskit.data.ratings.RatingVectorPDAO;
 import org.lenskit.results.Results;
+import org.lenskit.util.math.Vectors;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -62,16 +60,15 @@ public class SlopeOneItemScorer extends AbstractItemScorer {
     @Override
     public ResultMap scoreWithDetails(long user, @Nonnull Collection<Long> items) {
         Long2DoubleMap ratings = dao.userRatingVector(user);
-        SparseVector userVector = ImmutableSparseVector.create(ratings);
 
         List<Result> results = new ArrayList<>();
         LongIterator iter = LongIterators.asLongIterator(items.iterator());
         while (iter.hasNext()) {
             final long predicteeItem = iter.nextLong();
-            if (!userVector.containsKey(predicteeItem)) {
+            if (!ratings.containsKey(predicteeItem)) {
                 double total = 0;
                 int nitems = 0;
-                for (VectorEntry e: userVector) {
+                for (Long2DoubleMap.Entry e: Vectors.fastEntries(ratings)) {
                     long currentItem = e.getKey();
                     int nusers = model.getCoratings(predicteeItem, currentItem);
                     if (nusers != 0) {
