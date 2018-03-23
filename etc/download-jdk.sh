a5277a62713daa3ff5e53a676254f15fd45b47cf
Michael Ekstrand <michaelekstrand@boisestate.edu>
2018-03-23 12:26:16 -0600

    Add Java 10 to CI via manual download

---
+++ etc/download-jdk.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$custom_jdk" ]; then
+    echo "No custom JDK requested" >&2
+    exit
+fi
+
+tarfile=$(basename "$jdk_url")
+
+mkdir -p "$HOME/java/dist"
+cd "$HOME/java"
+if [ ! -f "$tarfile" ]; then
+    echo "Downloading $tarfile"
+    wget -O "$tarfile.tmp" "$jdk_url"
+    mv "$tarfile.tmp" "dist/$tarfile"
+fi
+
+tar xzf "dist/$tarfile"
+if [ ! -d "$custom_jdk" ]; then
+    echo "Unpacking $tarfile did not create $custom_jdk!" >&2
+    exit 1
+fi
